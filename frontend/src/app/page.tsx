59b3a95569c783649efec5b7fdc329fbd9f49239
bushwallyta271828 <aheckett@gmail.com>
2024-03-22 21:02:51 -0400

    Switched favicon for icon and it works (and modified page.tsx for testing)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="bg-white text-black">
       <h1 className="text-xl">Home</h1>
       <p>Some content</p>
+      <p>Some more content</p>
     </div>
   );
 }
