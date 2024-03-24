352be3015ea28cd9834cf42512adc114e22b0bea
bushwallyta271828 <aheckett@gmail.com>
2024-03-24 18:41:53 -0400

    IT WORKS!! IT FINALLY WORKS!
    Now time to figure out how much of the newly added stuff can be safely removed...

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,5 +3,6 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def getData(request):
+    print("HELLO, Django request = " + str(request))
     claim = {'id': 1, 'statement': 'The moon is made of cheese!'}
     return Response(claim)
