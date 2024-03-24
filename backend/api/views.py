abb783b462c0121b3609885a3b84b034fdc1aae8
bushwallyta271828 <aheckett@gmail.com>
2024-03-24 18:49:04 -0400

    Update views.py
    Removed print debug

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,5 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def getData(request):
-    print("HELLO, Django request = " + str(request))
     claim = {'id': 1, 'statement': 'The moon is made of cheese!'}
     return Response(claim)
