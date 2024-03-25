1ea2826285bdc76bc4b52c5a21048f1ac42d3c60
bushwallyta271828 <aheckett@gmail.com>
2024-03-25 14:47:36 -0400

    Created callapi.tsx
    I modified this code somewhat from the original version in claimbox.tsx, so we'll see if it still works.

---
+++ frontend/src/app/_lib/callapi.tsx
@@ -0,0 +1,12 @@
+import { unstable_noStore as noStore } from 'next/cache';
+
+export default async function CallAPI() {
+  noStore(); //Don't store process.env.BACKEND_ADDRESS.
+  try {
+    if (typeof process.env.BACKEND_ADDRESS !== 'undefined') {
+      return fetch(process.env.BACKEND_ADDRESS, { cache: 'no-store' });
+    }
+  } catch {
+    return null;
+  }
+}
