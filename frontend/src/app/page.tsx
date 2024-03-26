296684323c8ea065bb6dc54f3c077de763811eea
bushwallyta271828 <aheckett@gmail.com>
2024-03-26 12:51:18 -0400

    There's now appropriate spacing between claims.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,11 +4,11 @@ import CallAPI from "@/app/_lib/callapi";
 export default async function Home() {
   const responseText = await (await CallAPI()).text();
   return (
-    <>
+    <div className="flex flex-col p-4 gap-4">
       <ClaimBox text={responseText}/>
       <ClaimBox text={responseText}/>
       <ClaimBox text={responseText}/>
       <ClaimBox text={responseText}/>
-    </>
+    </div>
   );
 }
