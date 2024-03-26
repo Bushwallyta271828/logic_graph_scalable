d043580466a7e8593268906949d2b1177a2ece24
bushwallyta271828 <aheckett@gmail.com>
2024-03-25 22:20:27 -0400

    Colors somewhat better, still needs work

--- frontend/src/app/components/claimbox.tsx
+++ frontend/src/app/components/claimbox.tsx
@@ -4,23 +4,16 @@ type ClaimBoxProps = {
 
 export default function ClaimBox({text} : ClaimBoxProps) {
   return (
-    <div className="flex">
-      <div className="bg-blue-500 text-white w-8 p-4 rounded-l-md">
-        <pre>Hi!</pre>
+    <div className="flex shadow-xl">
+      <div className="bg-slate-800 text-white w-20 p-4 rounded-l-md">
+        <pre>9372</pre>
+        <pre>Text Claim</pre>
+        <pre>some_user</pre>
       </div>
-      <div className="bg-indigo-900 text-white flex-1 p-4 rounded-r-md border border-gray-200">
+      <div className="bg-indigo-950 text-white flex-1 p-4 rounded-r-md">
         <strong>Response:</strong>
         <pre>{text}</pre>
       </div>
     </div>
   );
-
-//
-//
-//
-//    <div className="bg-indigo-950 text-white max-w-xl mx-auto px-4 py-2 rounded-md shadow-md">
-//      <strong>Response:</strong>
-//      <pre>{text}</pre>
-//    </div>
-//  );
 }
