bbe6056906ade4985a7afecb24c7a06dfe210e43
bushwallyta271828 <aheckett@gmail.com>
2024-03-26 15:01:13 -0400

    Made the ClaimBox draggable (though you can't drag it anywhere...)

--- frontend/src/app/components/claimbox.tsx
+++ frontend/src/app/components/claimbox.tsx
@@ -4,10 +4,9 @@ type ClaimBoxProps = {
 
 export default function ClaimBox({text} : ClaimBoxProps) {
   return (
-    <div className="flex shadow-xl">
+    <div draggable="true" className="flex shadow-xl">
       <div className="bg-slate-800 text-white w-30 p-2 rounded-l-md text-ellipsis text-sm">
         <p>9372</p>
-        <p>Text Claim</p>
         <p>some_user</p>
       </div>
       <div className="bg-slate-900 text-white flex-1 p-2 rounded-r-md text-wrap text-sm">
