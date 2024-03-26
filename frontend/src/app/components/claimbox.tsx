cffb6438b0bdb7e87ccc61742c9b60d40aa59578
bushwallyta271828 <aheckett@gmail.com>
2024-03-25 21:55:15 -0400

    Got background color working!

--- frontend/src/app/components/claimbox.tsx
+++ frontend/src/app/components/claimbox.tsx
@@ -4,7 +4,7 @@ type ClaimBoxProps = {
 
 export default function ClaimBox({text} : ClaimBoxProps) {
   return (
-    <div className="bg-slate-900 text-white max-w-xl mx-auto px-4 py-2 rounded-md shadow-md">
+    <div className="bg-slate-800 text-white max-w-xl mx-auto px-4 py-2 rounded-md shadow-md">
       <strong>Response:</strong>
       <pre>{text}</pre>
     </div>
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/components/navbar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Logic Graph",
@@ -16,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="bg-neutral-800">
         <Navbar />
         {children}
       </body>
