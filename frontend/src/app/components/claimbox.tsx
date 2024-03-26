c8997070130807a789a366b62a824a12ced1b2ac
bushwallyta271828 <aheckett@gmail.com>
2024-03-26 11:43:16 -0400

    Claim dimensions are looking better now.

--- frontend/src/app/components/claimbox.tsx
+++ frontend/src/app/components/claimbox.tsx
@@ -5,14 +5,13 @@ type ClaimBoxProps = {
 export default function ClaimBox({text} : ClaimBoxProps) {
   return (
     <div className="flex shadow-xl">
-      <div className="bg-slate-800 text-white w-20 p-4 rounded-l-md">
-        <pre>9372</pre>
-        <pre>Text Claim</pre>
-        <pre>some_user</pre>
+      <div className="bg-slate-800 text-white w-30 p-2 rounded-l-md text-ellipsis text-sm">
+        <p>9372</p>
+        <p>Text Claim</p>
+        <p>some_user</p>
       </div>
-      <div className="bg-indigo-950 text-white flex-1 p-4 rounded-r-md">
-        <strong>Response:</strong>
-        <pre>{text}</pre>
+      <div className="bg-indigo-950 text-white flex-1 p-2 rounded-r-md text-wrap text-sm">
+        <p>{text}{text}{text}{text}{text}</p>
       </div>
     </div>
   );
