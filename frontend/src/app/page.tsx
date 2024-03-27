0696159a10a6f49b799a7a3f69d0bcebafa5e626
bushwallyta271828 <aheckett@gmail.com>
2024-03-27 16:00:35 -0400

    Added more claims to play with, should disable vertical scroll bar next.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,6 +20,54 @@ export default async function Home() {
     initialText: responseText,
     claimID: '1000',
     user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '2',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '20',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '200',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '2000',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '3',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '30',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '300',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '3000',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '4',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '40',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '400',
+    user: 'some_user'
+  },{
+    initialText: responseText,
+    claimID: '4000',
+    user: 'some_user'
   },];
   return (
     <ClaimList initialClaims={claims}/>
