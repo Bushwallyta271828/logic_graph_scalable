11aaa5692ab8a9b8ff4d888cf90925e2462c4465
bushwallyta271828 <aheckett@gmail.com>
2024-03-28 07:59:57 -0400

    Copied over ClaimList function and started integrating (not done yet)

--- frontend/src/app/components/claimbox.tsx
+++ frontend/src/app/components/claimbox.tsx
@@ -37,11 +37,40 @@ const DefinitionBox = ({definition, index, final}: {definition: DefinitionBoxPro
   );
 }
 
-type ClaimContentRegionProps = {
-  initialText: string;
-};
+function DefinitionList({definitions} : {definitions: DefinitionBoxProps[]}) {
+  const [claims, setClaims] = useState(initialClaims);
 
-function ClaimContentRegion({initialText} : ClaimContentRegionProps) {
+  function onDragEnd(result : DropResult) {
+    if (!result.destination || (result.destination.index === result.source.index)) {
+      return;
+    }
+
+    const newClaims = Array.from(claims);
+    const [removed] = newClaims.splice(result.source.index, 1);
+    newClaims.splice(result.destination.index, 0, removed);
+
+    setClaims(newClaims);
+  }
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="list">
+        {provided => (
+          <div className="flex flex-col p-4 gap-4"
+            ref={provided.innerRef}
+            {...provided.droppableProps}>
+            {claims.map((claim: ClaimBoxProps, index: number) => (
+              <MovableClaimBox claim={claim} index={index} key={claim.claimID} />))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+}
+
+
+function ClaimContentRegion({initialText} : {initialText : string}) {
   const [text, setText] = useState(initialText);
   const [isEditing, setIsEditing] = useState(false);
 
