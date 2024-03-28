46c7d27e5f6c9dc5c0980e13850674b8a1a6cfa3
bushwallyta271828 <aheckett@gmail.com>
2024-03-27 21:35:07 -0400

    Perfect! With the thin scrollbar and these colors, I think it looks great.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scrollbar scrollbar-track-gray-900 scrollbar-thumb-blue-950">
+    <html lang="en" className="scrollbar-thin scrollbar-track-slate-900 scrollbar-thumb-slate-800">
       <body className="bg-neutral-800">
         <NavBar />
         {children}
