1d21b5fef4a2b4cc50350d2353c8e9e6c9b80019
bushwallyta271828 <aheckett@gmail.com>
2024-03-30 12:18:16 -0400

    Added color config to tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,7 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text-claim-tab': '#000060',
+	'text-claim-body': '#000040',
+	'definition-claim-tab': '#006060',
+	'definition-claim-body': '#004040',
+      },
+    },
   },
   plugins: [
     require('tailwind-scrollbar'),
