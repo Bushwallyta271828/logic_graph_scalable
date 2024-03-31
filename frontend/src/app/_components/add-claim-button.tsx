eb89842a6f33a90e5d4c40fc1ad26271e0c12e85
bushwallyta271828 <aheckett@gmail.com>
2024-03-31 18:59:32 -0400

    In theory AddClaimButton should have a dropdown now.

--- frontend/src/app/_components/add-claim-button.tsx
+++ frontend/src/app/_components/add-claim-button.tsx
@@ -1,11 +1,69 @@
 'use client';
 
+import { useState, useRef, useEffect } from 'react';
 //import { addClaim } from '@app/_contexts/claims-context';
 
-export function AddClaimButton() {
+//export function AddClaimButton() {
+//  return (
+//    <button className="bg-transparent hover:bg-zinc-700 px-2 py-1 rounded">
+//      New Claim
+//    </button>
+//  );
+//}
+
+
+export function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => setIsOpen(!isOpen);
+  const dropdownRef = useRef(null);
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownRef]);
+
   return (
-    <button className="bg-transparent hover:bg-zinc-700 px-2 py-1 rounded">
-      New Claim
-    </button>
+    <div className="relative" ref={dropdownRef}>
+      <button
+        onClick={toggleDropdown}
+        className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+      >
+        New Claim
+      </button>
+      {isOpen && (
+        <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+          <a
+            key={index}
+            onClick={console.log('text')}
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            Text Claim
+          </a>
+          <a
+            key={index}
+            onClick={console.log('definition')}
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            Definition Claim
+          </a>
+          <a
+            key={index}
+            onClick={console.log('zeroth-order')}
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+          >
+            Zeroth Order Claim
+          </a>
+        </div>
+      )}
+    </div>
   );
-}
+};
