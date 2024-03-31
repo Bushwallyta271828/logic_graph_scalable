b95abe4bd24e1d01004b9524671d3cca48c3609b
bushwallyta271828 <aheckett@gmail.com>
2024-03-31 18:03:58 -0400

    Redid navbar styling to use buttons (incomplete)
    You have to click on the text to activate the link, not on the button in general. Will fix this next.

--- frontend/src/app/_components/navigation-bar.tsx
+++ frontend/src/app/_components/navigation-bar.tsx
@@ -7,21 +7,21 @@ export function NavigationBar() {
   const { claimLookup, claimIDs, setClaimLookup, setClaimIDs } = useClaimsContext();
 
   return (
-    <nav className="bg-zinc-900 text-white px-8 py-4">
+    <nav className="bg-zinc-900 text-white text-lg font-bold px-8 py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-bold">
-          <p>Hi!</p>
-        </div>
-        <div className="flex gap-8">
-          <div className="text-lg font-bold">
+        <button className="bg-transparent hover:bg-zinc-700 py-2 px-2 rounded">
+          <p>New Claim</p>
+        </button>
+        <div className="flex gap-4">
+          <button className="bg-transparent hover:bg-zinc-700 py-2 px-2 rounded">
             <Link href="/">Claims</Link>
-          </div>
-          <div className="text-lg font-bold">
+          </button>
+          <button className="bg-transparent hover:bg-zinc-700 py-2 px-2 rounded">
             <Link href="/">Analysis</Link>
-          </div>
-          <div className="text-lg font-bold">
+          </button>
+          <button className="bg-transparent hover:bg-zinc-700 py-2 px-2 rounded">
             <Link href="/documentation">Documentation</Link>
-          </div>
+          </button>
         </div>
       </div>
     </nav>
