2ba3fb45b1c20c527e85dda7d30e454856dd2399
bushwallyta271828 <aheckett@gmail.com>
2024-03-31 17:47:07 -0400

    Started giving NavigationBar a New Claim button.

--- frontend/src/app/_components/navigation-bar.tsx
+++ frontend/src/app/_components/navigation-bar.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import Link from 'next/link';
+import { useClaimsContext } from '@/app/_contexts/claims-context';
 
 export function NavigationBar() {
+  const { claimLookup, claimIDs, setClaimLookup, setClaimIDs } = useClaimsContext();
+
   return (
     <nav className="bg-zinc-900 text-white px-8 py-4">
       <div className="container mx-auto flex justify-between items-center">
+        <div className="text-lg font-bold">
+          <p>Hi!</p>
+        </div>
         <div className="flex gap-8">
           <div className="text-lg font-bold">
             <Link href="/">Claims</Link>
@@ -11,9 +19,9 @@ export function NavigationBar() {
           <div className="text-lg font-bold">
             <Link href="/">Analysis</Link>
           </div>
-        </div>
-        <div className="text-lg font-bold">
-          <Link href="/documentation">Documentation</Link>
+          <div className="text-lg font-bold">
+            <Link href="/documentation">Documentation</Link>
+          </div>
         </div>
       </div>
     </nav>
