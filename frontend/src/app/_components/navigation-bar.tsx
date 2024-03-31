c575af287c1612135711a88fa86c4a146e9034c4
bushwallyta271828 <aheckett@gmail.com>
2024-03-31 18:18:37 -0400

    Buttons look good now!
    Now let's work on New Claim!

--- frontend/src/app/_components/navigation-bar.tsx
+++ frontend/src/app/_components/navigation-bar.tsx
@@ -9,22 +9,22 @@ export function NavigationBar() {
   return (
     <nav className="bg-zinc-900 text-white text-lg font-bold px-8 py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <button className="bg-transparent hover:bg-zinc-700 py-2 px-2 rounded">
+        <button className="bg-transparent hover:bg-zinc-700 px-2 py-1 rounded">
           <p>New Claim</p>
         </button>
         <div className="flex gap-4">
           <Link href="/">
-            <button className="bg-transparent hover:bg-zinc-700 py-2 px-2 rounded">
+            <button className="bg-transparent hover:bg-zinc-700 px-2 py-1 rounded">
               Claims
             </button>
           </Link>
           <Link href="/">
-            <button className="bg-transparent hover:bg-zinc-700 py-2 px-2 rounded">
+            <button className="bg-transparent hover:bg-zinc-700 px-2 py-1 rounded">
               Analysis
             </button>
           </Link>
           <Link href="/documentation">
-            <button className="bg-transparent hover:bg-zinc-700 py-2 px-2 rounded">
+            <button className="bg-transparent hover:bg-zinc-700 px-2 py-1 rounded">
               Documentation
             </button>
           </Link>
