fd9ce1002a3045c108eebb514a7a279e3cc616bb
bushwallyta271828 <aheckett@gmail.com>
2024-04-01 09:35:54 -0400

    Rounding now works without padding

--- frontend/src/app/_components/add-claim-button.tsx
+++ frontend/src/app/_components/add-claim-button.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-//<button className="bg-transparent hover:bg-zinc-700 px-2 py-1 rounded">
-
 import { Menu } from '@headlessui/react'
 
 export function AddClaimButton() {
@@ -12,17 +10,17 @@ export function AddClaimButton() {
           New Claim
         </Menu.Button>
         <Menu.Items className="absolute z-10 mt-2 w-48 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1"> {/* Wrapper to enforce vertical layout */}
+          <div>
             <Menu.Item>
               {({ active }) => (
-                <a className={`block px-4 py-2 text-sm ${active ? 'bg-blue-500 text-white' : 'text-gray-700'}`}>
+                <a className={`block px-4 py-2 text-sm rounded-t-md ${active ? 'bg-blue-500 text-white' : 'text-gray-700'}`}>
                   Text Claim
                 </a>
               )}
             </Menu.Item>
             <Menu.Item>
               {({ active }) => (
-                <a className={`block px-4 py-2 text-sm ${active ? 'bg-blue-500 text-white' : 'text-gray-700'}`}>
+                <a className={`block px-4 py-2 text-sm rounded-b-md ${active ? 'bg-blue-500 text-white' : 'text-gray-700'}`}>
                   Definition Claim
                 </a>
               )}
