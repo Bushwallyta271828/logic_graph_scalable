771ab2fa7cac814335d7201f4fbfd16efc5a7dfc
bushwallyta271828 <aheckett@gmail.com>
2024-04-01 09:24:47 -0400

    Fixed button styling

--- frontend/src/app/_components/add-claim-button.tsx
+++ frontend/src/app/_components/add-claim-button.tsx
@@ -8,7 +8,7 @@ export function AddClaimButton() {
   return (
     <div className="relative">
       <Menu>
-        <Menu.Button className="inline-flex justify-center w-full px-2 py-1 text-sm font-medium text-white bg-blue-500 rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        <Menu.Button className="bg-transparent hover:bg-zinc-700 px-2 py-1 rounded">
           New Claim
         </Menu.Button>
         <Menu.Items className="absolute z-10 mt-2 w-48 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
