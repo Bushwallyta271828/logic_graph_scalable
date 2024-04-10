60b4f71ddf0c6b69349a58e1c79f983c0e2a2ddc
bushwallyta271828 <aheckett@gmail.com>
2024-04-10 11:58:07 -0400

    Removed relative div for ClaimTab

--- frontend/src/app/_components/claim-box.tsx
+++ frontend/src/app/_components/claim-box.tsx
@@ -14,37 +14,35 @@ function ClaimTab({claim} : {claim: Claim}) {
   //const { attachDefinition, deleteClaim } = useClaimsContext();
 
   return (
-    <div className="relative">
-      <Menu>
-        <Menu.Button className={`${claim.claimType === 'text' ? 'bg-medium-text hover:bg-bright-text' : claim.claimType === 'definition' ? 'bg-medium-definition hover:bg-bright-definition' : 'bg-medium-zeroth-order hover:bg-bright-zeroth-order'} w-20 p-2 rounded-l-md`}>
-          <p className="text-white text-sm truncate">{claim.claimID}</p>
-          <p className="text-white text-sm truncate">{claim.author}</p>
-        </Menu.Button>
-        <Menu.Items className="absolute w-64 origin-top-right bg-transparent outline-none rounded-md shadow-xl text-sm font-normal">
-          <div>
-            {acceptsDefinitions ? 
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    className={`block px-4 py-2 rounded-t-md ${active ? 'bg-bright-definition' : 'bg-medium-definition'}`}>
-                    Attach Definition
-                  </a>
-                )}
-              </Menu.Item> :
-              null
-            }
+    <Menu>
+      <Menu.Button className={`${claim.claimType === 'text' ? 'bg-medium-text hover:bg-bright-text' : claim.claimType === 'definition' ? 'bg-medium-definition hover:bg-bright-definition' : 'bg-medium-zeroth-order hover:bg-bright-zeroth-order'} w-20 p-2 rounded-l-md`}>
+        <p className="text-white text-sm truncate">{claim.claimID}</p>
+        <p className="text-white text-sm truncate">{claim.author}</p>
+      </Menu.Button>
+      <Menu.Items className="absolute w-64 origin-top-right bg-transparent outline-none rounded-md shadow-xl text-sm font-normal">
+        <div>
+          {acceptsDefinitions ? 
             <Menu.Item>
               {({ active }) => (
                 <a
-                  className={`block px-4 py-2 ${acceptsDefinitions ? 'rounded-b-md' : 'rounded-md'} ${active ? 'bg-bright-danger' : 'bg-medium-danger'}`}>
-                  Delete Claim
+                  className={`block px-4 py-2 rounded-t-md ${active ? 'bg-bright-definition' : 'bg-medium-definition'}`}>
+                  Attach Definition
                 </a>
               )}
-            </Menu.Item>
-          </div>
-        </Menu.Items>
-      </Menu>
-    </div>
+            </Menu.Item> :
+            null
+          }
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`block px-4 py-2 ${acceptsDefinitions ? 'rounded-b-md' : 'rounded-md'} ${active ? 'bg-bright-danger' : 'bg-medium-danger'}`}>
+                Delete Claim
+              </a>
+            )}
+          </Menu.Item>
+        </div>
+      </Menu.Items>
+    </Menu>
   );
 }
 
