012398988b651a99870bf9e8e21462f28b4f3325
bushwallyta271828 <aheckett@gmail.com>
2024-04-12 18:21:14 -0400

    Fixed compilation issue

--- frontend/src/app/_components/static-content-box.tsx
+++ frontend/src/app/_components/static-content-box.tsx
@@ -7,11 +7,11 @@ function TextContentBox({textClaim} : {textClaim: TextClaim}) {
 }
 
 function DefinitionContentBox({definitionClaim} : {definitionClaim: DefinitionClaim}) {
-  return (<p className="text-white text-sm break-words">"{definitionClaim.text}" is a valid definition.</p>);
+  return (<p className="text-white text-sm break-words">{definitionClaim.text} is a valid definition.</p>);
 }
 
 function ZerothOrderContentBox({zerothOrderClaim} : {zerothOrderClaim: ZerothOrderClaim}) {
-  return (<p className="text-white text-sm break-words">"{zerothOrderClaim.text}" is a formula I will soon parse.</p>);
+  return (<p className="text-white text-sm break-words">{zerothOrderClaim.text} is a formula I will soon parse.</p>);
 }
 
 export function StaticContentBox({claim} : {claim: Claim}) {
