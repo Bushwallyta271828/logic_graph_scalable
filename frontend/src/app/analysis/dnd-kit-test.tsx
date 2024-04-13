f531d6569142eb49b1e0788eada9b1f4ab8cb907
bushwallyta271828 <aheckett@gmail.com>
2024-04-13 16:41:29 -0400

    Trying to use button with sensors (doesn't compile yet)

--- frontend/src/app/analysis/dnd-kit-test.tsx
+++ frontend/src/app/analysis/dnd-kit-test.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { DndContext, closestCorners, DragEndEvent } from '@dnd-kit/core';
 import { SortableContext, verticalListSortingStrategy, useSortable, arrayMove } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
+import { Menu } from '@headlessui/react'
 
 function Task({id, title}: {id:number, title: string}) {
   const {attributes, listeners, setNodeRef, transform, transition} = useSortable({id});
@@ -20,6 +21,34 @@ function Task({id, title}: {id:number, title: string}) {
       {...listeners}
       style={style}>
       {title}
+      <div className="relative">
+      <Menu>
+        <Menu.Button className={`bg-medium-text hover:bg-bright-text h-full w-20 p-2 rounded-l-md`}>
+          <p className="text-white text-sm truncate">123456</p>
+          <p className="text-white text-sm truncate">local</p>
+        </Menu.Button>
+        <Menu.Items className="absolute w-40 origin-top-right z-10 bg-transparent outline-none rounded-md shadow-xl text-white text-sm font-normal">
+          <div>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`block px-4 py-2 rounded-t-md ${active ? 'bg-bright-definition' : 'bg-medium-definition'}`}>
+                    Attach Definition
+                  </a>
+                )}
+              </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  className={`block px-4 py-2 rounded-b-md ${active ? 'bg-bright-danger' : 'bg-medium-danger'}`}>
+                  Delete Claim
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Menu>
+    </div>
     </div>
   );
 }
@@ -44,8 +73,16 @@ export function DndKitTest() {
     }
   }
 
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    })
+  )
+
   return (
-    <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+    <DndContext sensors={sensors} collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
       <div className="column">
         <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
         {tasks.map((task) => (
