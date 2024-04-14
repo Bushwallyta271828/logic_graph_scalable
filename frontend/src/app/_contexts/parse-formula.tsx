2f3815aec5f2aaa32d9e26f1da24ade0c8bd6024
bushwallyta271828 <aheckett@gmail.com>
2024-04-14 19:58:31 -0400

    Worked on SeparateParentheticals

--- frontend/src/app/_contexts/parse-formula.tsx
+++ frontend/src/app/_contexts/parse-formula.tsx
@@ -1,12 +1,32 @@
 'use client';
 
+function SeparateParentheticals({formula}: {formula:string}) {
+  //This function 
+  let topLevelFormula = "";
+  let children: string[] = [];
+  let depth = 0;
+  const matching = true;
+  for (let i = 0; i < formula.length; i++) {
+    if (depth >= 1) {
+      children[children.length - 1] += formula[i];
+    }
+    if (formula[i] === "(") {
+      if (depth === 0) {parentheticals.push("");}
+      depth += 1;
+    }
+    
+  }
+  return {topLevelFormula: topLevelFormula, children: children, matching: matching};
+}
+
 function ParseLogicalFormula({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
   
 }
 
-function ParseEquationSide({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
-  let first_plus = -1;
-  let parenthetical_depth = 0;
+function ParseAffineFormula({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
+  let 
+  let parentheticalDepth = 0;
+  
   let i = 0;
   while 
   for (let i = 0; i < formula.length; i++) {
@@ -17,15 +37,18 @@ function ParseEquationSide({formula,substitutions}:{formula:string,substitutions
 export function ParseFormula({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
   if (formula.includes("=")) {
     const equationSides = formula.split("=");
-    if (equationSides.length !== 2)
-      {return {substitutedFormula: formula, validFormula: false};}
-    const {substitutedFormula: leftSubstitutedFormula, validFormula: leftValidFormula}
-      = ParseEquationSide({formula: equationSides[0], substitutions: substitutions});
-    const {substitutedFormula: rightSubstitutedFormula, validFormula: rightValidFormula}
-      = ParseEquationSide({formula: equationSides[1], substitutions: substitutions});
-    return {
-      substitutedFormula: leftSubstitutedFormula+"="+rightSubstitutedFormula,
-      validFormula: leftValidFormula && rightValidFormula};
+    if (equationSides.length !== 2) {
+      return {substitutedFormula: formula, validFormula: false};
+    } else {
+      const {substitutedFormula: leftSubstitutedFormula, validFormula: leftValidFormula}
+        = ParseAffineFormula({formula: equationSides[0], substitutions: substitutions});
+      const {substitutedFormula: rightSubstitutedFormula, validFormula: rightValidFormula}
+        = ParseAffineFormula({formula: equationSides[1], substitutions: substitutions});
+      return {
+        substitutedFormula: leftSubstitutedFormula+"="+rightSubstitutedFormula,
+        validFormula: leftValidFormula && rightValidFormula
+      };
+    }
   } else {
     return ParseLogicalFormula({formula:formula, substitutions:substitutions});
   }
