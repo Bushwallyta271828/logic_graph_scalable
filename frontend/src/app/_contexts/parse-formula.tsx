6efbfd0e42a6404917a1fb2849faecbfd82b2a5f
bushwallyta271828 <aheckett@gmail.com>
2024-04-14 19:02:59 -0400

    Wrote ParseFormula main function
    Of course, most of the work is in the helper functions.

--- frontend/src/app/_contexts/parse-formula.tsx
+++ frontend/src/app/_contexts/parse-formula.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-export function ParseLogicalFormula({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
+function ParseLogicalFormula({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
   
 }
 
-export function ParseEquationSide({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
+function ParseEquationSide({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
   let first_plus = -1;
   let parenthetical_depth = 0;
   let i = 0;
@@ -16,7 +16,17 @@ export function ParseEquationSide({formula,substitutions}:{formula:string,substi
 
 export function ParseFormula({formula,substitutions}:{formula:string,substitutions:{[claimID:string]:string}}) {
   if (formula.includes("=")) {
-    
+    const equationSides = formula.split("=");
+    if (equationSides.length !== 2)
+      {return {substitutedFormula: formula, validFormula: false};}
+    const {substitutedFormula: leftSubstitutedFormula, validFormula: leftValidFormula}
+      = ParseEquationSide({formula: equationSides[0], substitutions: substitutions});
+    const {substitutedFormula: rightSubstitutedFormula, validFormula: rightValidFormula}
+      = ParseEquationSide({formula: equationSides[1], substitutions: substitutions});
+    return {
+      substitutedFormula: leftSubstitutedFormula+"="+rightSubstitutedFormula,
+      validFormula: leftValidFormula && rightValidFormula};
   } else {
+    return ParseLogicalFormula({formula:formula, substitutions:substitutions});
   }
 }
