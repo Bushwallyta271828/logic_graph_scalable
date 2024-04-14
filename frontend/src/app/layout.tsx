999937856684d86c973a2c63aa92ebb64a88fbff
bushwallyta271828 <aheckett@gmail.com>
2024-04-14 11:51:19 -0400

    Removed MathJax references

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from 'next';
 import './globals.css';
-//import { MathJaxContext } from 'better-react-mathjax';
 import { ClaimsContextProvider } from '@/app/_contexts/claims-context';
 import { NavigationBar } from '@/app/_components/navigation-bar';
 
 export const metadata: Metadata = {
   title: 'Logic Graph',
-};
-
-const MathJaxConfig = {
-  loader: { load: ['input/asciimath'] },
-  asciimath: { delimiters: [["$$","$$"]] },
-  tex: {
-    inlineMath: [['$', '$']],
-    displayMath: [['$$', '$$']],
-  },
 };
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
