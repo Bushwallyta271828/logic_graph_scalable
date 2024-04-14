e3ec8ef0e62b1d4587459fc061393af71ddeeb3d
bushwallyta271828 <aheckett@gmail.com>
2024-04-14 11:09:42 -0400

    Using MathJaxContext (doesn't compile)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-//import { MathJaxContext } from 'better-react-mathjax';
+import { MathJaxContext } from 'better-react-mathjax';
 import { ClaimsContextProvider } from '@/app/_contexts/claims-context';
 import { NavigationBar } from '@/app/_components/navigation-bar';
 
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'Logic Graph',
 };
 
-const MathJaxConfig = {
+const mathJaxConfig = {
   loader: { load: ['input/asciimath'] },
   asciimath: { delimiters: [["$$","$$"]] },
   tex: {
@@ -21,10 +21,12 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
   return (
     <html lang="en" className="scrollbar-thin scrollbar-track-dark-neutral scrollbar-thumb-medium-neutral">
       <body className="bg-medium-neutral">
-        <ClaimsContextProvider>
-          <NavigationBar />
-          {children}
-        </ClaimsContextProvider>
+        <MathJaxContext config={mathJaxConfig}>
+          <ClaimsContextProvider>
+            <NavigationBar />
+            {children}
+          </ClaimsContextProvider>
+        </MathJaxContext>
       </body>
     </html>
   );
