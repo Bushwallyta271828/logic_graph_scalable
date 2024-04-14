67d0568c1553ed810263f38f55fc399a9d5dc713
bushwallyta271828 <aheckett@gmail.com>
2024-04-13 21:44:25 -0400

    Removed dnd-kit-test from analysis

--- frontend/src/app/analysis/page.tsx
+++ frontend/src/app/analysis/page.tsx
@@ -1,10 +1,7 @@
-import { DndKitTest } from '@/app/analysis/dnd-kit-test';
-
 export default function Analysis() {
   return (
     <div className="bg-red-800 text-white">
       <h1 className="text-xl">Welcome to the analysis of the claims!</h1>
-      <DndKitTest />
     </div>
   );
 }
