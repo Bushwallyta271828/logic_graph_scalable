b48c9ff69b77f1cf02c86fc14c039c9600b91e0c
bushwallyta271828 <aheckett@gmail.com>
2024-04-20 14:52:57 -0400

    Fixed capitalization on helper functions

--- frontend/src/app/_contexts/immediate-constraint-dependencies.tsx
+++ frontend/src/app/_contexts/immediate-constraint-dependencies.tsx
@@ -9,27 +9,27 @@ import {
   ConstraintParse
 } from '@/app/_types/parse-types.tsx';
 
-function LogicalFormulaDependencies({parse}: {parse: LogicalFormula}): Set<string> {
+function logicalFormulaDependencies({parse}: {parse: LogicalFormula}): Set<string> {
   switch (parse.parseType) {
     case 'LogicalFormulaImplies':
-      const left = LogicalFormulaDependencies({parse: parse.left});
-      const right = LogicalFormulaDependencies({parse: parse.right});
+      const left = logicalFormulaDependencies({parse: parse.left});
+      const right = logicalFormulaDependencies({parse: parse.right});
       return new Set([...left, ...right]);
     case 'LogicalFormulaOr':
     case 'LogicalFormulaAnd':
     case 'LogicalFormulaNot':
-      return LogicalFormulaDependencies({parse: parse.child});
+      return logicalFormulaDependencies({parse: parse.child});
     case 'ClaimID':
       return new Set([parse.claimID,]);
     default: throw new Error("Unrecognized parseType");
   }
 }
 
-function LogicalFormulaWithoutImpliesDependencies({parse}:
+function logicalFormulaWithoutImpliesDependencies({parse}:
   {parse: LogicalFormulaWithoutImplies}): Set<string> {
 }
 
-function AffineExpressionDependencies({parse}: {parse: AffineExpression}): Set<string> {
+function affineExpressionDependencies({parse}: {parse: AffineExpression}): Set<string> {
 }
 
 export function immediateConstraintDependencies({parse}: {parse: ConstraintParse}): Set<string> {
