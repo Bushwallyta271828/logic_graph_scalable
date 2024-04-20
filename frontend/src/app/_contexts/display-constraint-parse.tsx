483e02823b6f4f489f1e7f4135d14078e28f42bf
bushwallyta271828 <aheckett@gmail.com>
2024-04-20 11:28:40 -0400

    Finished displayLogicalFormulaWithoutImplies

--- frontend/src/app/_contexts/display-constraint-parse.tsx
+++ frontend/src/app/_contexts/display-constraint-parse.tsx
@@ -30,11 +30,18 @@ export function displayLogicalFormulaWithoutImplies({parse, substitutions}:
         wrap: (child.parseType === 'LogicalFormulaWithoutImpliesOr'),
         text: displayLogicalFormulaWithoutImplies(
           {parse: child, substitutions: substitutions}),
-        });
+      }));
       return subDisplays.join(' and ');
     case 'LogicalFormulaWithoutImpliesNot':
-    case 'ClaimID': 
-      
+      return "not " + maybeWrap({
+        wrap: ['LogicalFormulaWithoutImpliesOr', 'LogicalFormulaWithoutImpliesAnd']
+          .includes(parse.child.parseType),
+        text: displayLogicalFormulaWithoutImplies(
+          {parse: parse.child, substitutions: substitutions}),
+      });
+    case 'ClaimID':
+      if (!substitutions.has(claim.value)) {throw new Error("Unrecognized claim ID");}
+      return "["+claim.value+": "+substitutions[claim.value]+"]";
     default: throw new Error('Unrecognized parseType');
   }
 }
