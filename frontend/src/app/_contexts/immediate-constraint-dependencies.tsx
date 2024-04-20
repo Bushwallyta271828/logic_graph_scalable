8665c47c6b545a45a9bc8fc945281961f5837921
bushwallyta271828 <aheckett@gmail.com>
2024-04-20 14:51:57 -0400

    Declared helper functions and worked on some implementation

--- frontend/src/app/_contexts/immediate-constraint-dependencies.tsx
+++ frontend/src/app/_contexts/immediate-constraint-dependencies.tsx
@@ -9,6 +9,28 @@ import {
   ConstraintParse
 } from '@/app/_types/parse-types.tsx';
 
-export function immediateConstraintDependencies({parse}: {parse: ConstraintParse}) {
-  //TODO
+function LogicalFormulaDependencies({parse}: {parse: LogicalFormula}): Set<string> {
+  switch (parse.parseType) {
+    case 'LogicalFormulaImplies':
+      const left = LogicalFormulaDependencies({parse: parse.left});
+      const right = LogicalFormulaDependencies({parse: parse.right});
+      return new Set([...left, ...right]);
+    case 'LogicalFormulaOr':
+    case 'LogicalFormulaAnd':
+    case 'LogicalFormulaNot':
+      return LogicalFormulaDependencies({parse: parse.child});
+    case 'ClaimID':
+      return new Set([parse.claimID,]);
+    default: throw new Error("Unrecognized parseType");
+  }
+}
+
+function LogicalFormulaWithoutImpliesDependencies({parse}:
+  {parse: LogicalFormulaWithoutImplies}): Set<string> {
+}
+
+function AffineExpressionDependencies({parse}: {parse: AffineExpression}): Set<string> {
+}
+
+export function immediateConstraintDependencies({parse}: {parse: ConstraintParse}): Set<string> {
 }
