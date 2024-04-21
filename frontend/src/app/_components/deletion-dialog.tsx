370ed2d89924c17c3f2fbbbc672784c9a1c7823f
bushwallyta271828 <aheckett@gmail.com>
2024-04-21 19:36:07 -0400

    Added buttons (not in the right place yet).

--- frontend/src/app/_components/deletion-dialog.tsx
+++ frontend/src/app/_components/deletion-dialog.tsx
@@ -19,7 +19,7 @@ export function DeletionDialog({dialogOpen, setDialogOpen, claimsToDelete}: {
         <Dialog.Panel className="mx-auto max-w-lg rounded-md outline outline-2 outline-white bg-dark-neutral text-white p-2">
           <Dialog.Title className="text-center text-lg font-bold">Deleting Additional Claims</Dialog.Title>
           <p>
-            By deleting this claim, you will also be deleting the following claims which depend on it:
+            By deleting this claim, you will also be deleting the following claims which depend upon it:
           </p>
           <div className="p-2">
             { claimsToDelete.slice(0, 8).map((claimToDelete, index) => (
@@ -30,7 +30,15 @@ export function DeletionDialog({dialogOpen, setDialogOpen, claimsToDelete}: {
             }
             {(claimsToDelete.length > 8) ? <p key={8}>...</p> : null}
           </div>
-          <p>Are you sure you want to proceed?</p>
+          <p>Are you sure you wish to proceed?</p>
+          <div className="min-w-full flex-row justify-between">
+            <button className="bg-medium-neutral hover:bg-light-neutral text-lg font-bold px-2 py-1 rounded-md">
+              Cancel
+            </button>
+            <button className="bg-medium-danger hover:bg-light-danger text-lg font-bold px-2 py-1 rounded-md">
+              Delete
+            </button>
+          </div>
         </Dialog.Panel>
       </div>
     </Dialog>
