ac047a880c088958183cb4ce9297722357f151dc
bushwallyta271828 <aheckett@gmail.com>
2024-04-21 17:50:12 -0400

    Added claimsToDelete argument

--- frontend/src/app/_components/deletion-dialog.tsx
+++ frontend/src/app/_components/deletion-dialog.tsx
@@ -1,30 +1,30 @@
 'use client';
 
-import { Dialog } from '@headlessui/react'
+import { Dialog } from '@headlessui/react';
+import { Claim } from '@/app/_types/claim-types';
 
 
-export function DeletionDialog({dialogOpen, setDialogOpen}: {
+export function DeletionDialog({dialogOpen, setDialogOpen, claimsToDelete}: {
   dialogOpen: boolean,
-  setDialogOpen: React.Dispatch<React.SetStateAction<boolean>>
+  setDialogOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  claimsToDelete: Claim[],
 }) {
   return (
     <Dialog
       open={dialogOpen}
       onClose={() => setDialogOpen(false)}
-      className="relative z-50"
-    >
-      {/* The backdrop, rendered as a fixed sibling to the panel container */}
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-
-      {/* Full-screen container to center the panel */}
+      className="relative z-50">
+      <div className="fixed inset-0 bg-black/30 backdrop-blur" aria-hidden="true" />
       <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-        {/* The actual dialog panel  */}
-        <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-          <Dialog.Title>Complete your order</Dialog.Title>
-
-          {/* ... */}
+        <Dialog.Panel className="mx-auto max-w-sm rounded-md bg-bright-neutral text-white">
+          <Dialog.Title>Deleting Additional Claims</Dialog.Title>
+          <p>
+            By deleting this claim, you will also be deleting the following claims which depend on it:
+          </p>
+          {claimsToDelete.map((claimToDelete) => (<p>{claimToDelete.text}</p>)}
+          <p>Are you sure you want to proceed?</p>
         </Dialog.Panel>
       </div>
     </Dialog>
-  )
+  );
 }
