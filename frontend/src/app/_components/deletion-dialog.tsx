ad09c901eecabe749efcd0404d759cc44d0a7af8
bushwallyta271828 <aheckett@gmail.com>
2024-04-21 18:08:41 -0400

    Added keys to make code compile

--- frontend/src/app/_components/deletion-dialog.tsx
+++ frontend/src/app/_components/deletion-dialog.tsx
@@ -9,19 +9,21 @@ export function DeletionDialog({dialogOpen, setDialogOpen, claimsToDelete}: {
   setDialogOpen: React.Dispatch<React.SetStateAction<boolean>>,
   claimsToDelete: Claim[],
 }) {
+  //Assumes claimsToDelete doesn't have any duplicates --
+  //otherwise we could get duplicate keys!
   return (
     <Dialog
       open={dialogOpen}
       onClose={() => setDialogOpen(false)}
       className="relative z-50">
       <div className="fixed inset-0 bg-black/30 backdrop-blur" aria-hidden="true" />
-      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-        <Dialog.Panel className="mx-auto max-w-sm rounded-md bg-bright-neutral text-white">
+      <div className="fixed inset-0 flex w-screen items-center justify-center">
+        <Dialog.Panel className="mx-auto max-w-sm rounded-md bg-bright-neutral text-white p-2">
           <Dialog.Title>Deleting Additional Claims</Dialog.Title>
           <p>
             By deleting this claim, you will also be deleting the following claims which depend on it:
           </p>
-          {claimsToDelete.map((claimToDelete) => (<p>{claimToDelete.text}</p>)}
+          {claimsToDelete.map((claimToDelete) => (<p key={claimToDelete.claimID}>{claimToDelete.text}</p>))}
           <p>Are you sure you want to proceed?</p>
         </Dialog.Panel>
       </div>
