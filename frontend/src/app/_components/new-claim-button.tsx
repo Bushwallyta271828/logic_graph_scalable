928a4d8f02a1b31ce41d9d6984b809c7171a9919
bushwallyta271828 <aheckett@gmail.com>
2024-04-22 11:12:54 -0400

    Fixed z-indexing

--- frontend/src/app/_components/new-claim-button.tsx
+++ frontend/src/app/_components/new-claim-button.tsx
@@ -16,7 +16,7 @@ export function NewClaimButton() {
               : (<p className="px-2 py-1 rounded-md bg-transparent hover:bg-medium-neutral">New Claim</p>) 
           }
         </Menu.Button>
-        <Menu.Items className="absolute w-40 origin-top-right z-20 bg-transparent outline-none rounded-md shadow-xl text-sm font-normal">
+        <Menu.Items className="absolute w-40 origin-top-right z-30 bg-transparent outline-none rounded-md shadow-xl text-sm font-normal">
           <div>
             <Menu.Item>
               {({ active }) => (
