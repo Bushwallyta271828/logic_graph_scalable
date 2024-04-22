0e51e2f70c8b4d5362caf38b1a6813d1ff1dec64
bushwallyta271828 <aheckett@gmail.com>
2024-04-22 11:56:18 -0400

    Added buttons for False, None, True

--- frontend/src/app/_components/claim-tab.tsx
+++ frontend/src/app/_components/claim-tab.tsx
@@ -35,9 +35,23 @@ export function ClaimTab({claim} : {claim: Claim}) {
           <Popover.Button className={`${claim.claimType === 'text' ? 'bg-medium-text hover:bg-bright-text' : claim.claimType === 'definition' ? 'bg-medium-definition hover:bg-bright-definition' : 'bg-medium-zeroth-order hover:bg-bright-zeroth-order'} h-full w-14 p-2 rounded-l-md`}>
             <p className="text-white text-sm truncate">{claim.claimID}</p>
           </Popover.Button>
-          <Popover.Panel className={`absolute w-40 origin-top-right z-20 bg-bright-neutral outline-none rounded-md shadow-xl text-white text-sm font-normal`}>
+          <Popover.Panel className={`absolute w-96 origin-top-right z-20 bg-bright-neutral outline-none rounded-md shadow-xl text-white text-sm font-normal`}>
             <div>
               <p className="px-4 py-2">Conditioning:</p>
+              <div className="container mx-auto flex justify-between items-center">
+                <Popover.Button
+                  className="block px-4 py-2 bg-bright-neutral hover:bg-medium-neutral">
+                  False
+                </Popover.Button>
+                <Popover.Button
+                  className="block px-4 py-2 bg-bright-neutral hover:bg-medium-neutral">
+                  None
+                </Popover.Button>
+                <Popover.Button
+                  className="block px-4 py-2 bg-bright-neutral hover:bg-medium-neutral">
+                  True
+                </Popover.Button>
+              </div>
               {acceptsDefinitions ? 
                 <Popover.Button
                   as="p"
