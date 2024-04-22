a1b496cc35ee5394c27de1dd82fb0de45cd81dc4
bushwallyta271828 <aheckett@gmail.com>
2024-04-22 10:59:56 -0400

    Changed Menu to Popover, it mostly works but a few things are broken.

--- frontend/src/app/_components/claim-tab.tsx
+++ frontend/src/app/_components/claim-tab.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Menu } from '@headlessui/react';
+import { Popover } from '@headlessui/react';
 import { Claim } from '@/app/_types/claim-types';
 import { useClaimsContext } from '@/app/_contexts/claims-context';
 import { DeletionDialog } from '@/app/_components/deletion-dialog';
@@ -31,36 +31,28 @@ export function ClaimTab({claim} : {claim: Claim}) {
   return (
     <>
       <div className="relative">
-        <Menu>
-          <Menu.Button className={`${claim.claimType === 'text' ? 'bg-medium-text hover:bg-bright-text' : claim.claimType === 'definition' ? 'bg-medium-definition hover:bg-bright-definition' : 'bg-medium-zeroth-order hover:bg-bright-zeroth-order'} h-full w-14 p-2 rounded-l-md`}>
+        <Popover>
+          <Popover.Button className={`${claim.claimType === 'text' ? 'bg-medium-text hover:bg-bright-text' : claim.claimType === 'definition' ? 'bg-medium-definition hover:bg-bright-definition' : 'bg-medium-zeroth-order hover:bg-bright-zeroth-order'} h-full w-14 p-2 rounded-l-md`}>
             <p className="text-white text-sm truncate">{claim.claimID}</p>
-          </Menu.Button>
-          <Menu.Items className={`absolute w-40 origin-top-right z-20 bg-transparent outline-none rounded-md shadow-xl text-white text-sm font-normal`}>
+          </Popover.Button>
+          <Popover.Panel className={`absolute w-40 origin-top-right z-20 bg-transparent outline-none rounded-md shadow-xl text-white text-sm font-normal`}>
             <div>
               {acceptsDefinitions ? 
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      className={`block px-4 py-2 rounded-t-md ${active ? 'bg-bright-definition' : 'bg-medium-definition'}`}
-                      onClick={() => attachBlankDefinition(claim)}>
-                      Attach Definition
-                    </a>
-                  )}
-                </Menu.Item> :
+                <p
+                  className={`block px-4 py-2 rounded-t-md ${false ? 'bg-bright-definition' : 'bg-medium-definition'}`}
+                  onClick={() => attachBlankDefinition(claim)}>
+                  Attach Definition
+                </p> :
                 null
               }
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    className={`block px-4 py-2 ${acceptsDefinitions ? 'rounded-b-md' : 'rounded-md'} ${active ? 'bg-bright-danger' : 'bg-medium-danger'}`}
-                    onClick={handleDelete}>
-                    Delete Claim
-                  </a>
-                )}
-              </Menu.Item>
+              <p
+                className={`block px-4 py-2 ${acceptsDefinitions ? 'rounded-b-md' : 'rounded-md'} ${false ? 'bg-bright-danger' : 'bg-medium-danger'}`}
+                onClick={handleDelete}>
+                Delete Claim
+              </p>
             </div>
-          </Menu.Items>
-        </Menu>
+          </Popover.Panel>
+        </Popover>
       </div>
       <DeletionDialog
         dialogOpen={dialogOpen}
