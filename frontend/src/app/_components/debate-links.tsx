e3b0c2bd278f09ab9676507ccac5c5a3237160fb
bushwallyta271828 <aheckett@gmail.com>
2024-04-23 11:12:07 -0400

    Created debate-links.tsx

---
+++ frontend/src/app/_components/debate-links.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export function DebateBar() {
+  return (
+    <div className="text-white text-lg font-bold flex gap-4">
+      <Link href="/">
+        <button className="bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
+          Claims
+        </button>
+      </Link>
+      <Link href="/analysis">
+        <button className="bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
+          Analysis
+        </button>
+      </Link>
+      <Link href="/documentation">
+        <button className="bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
+          Documentation
+        </button>
+      </Link>
+    </div>
+  );
+}
