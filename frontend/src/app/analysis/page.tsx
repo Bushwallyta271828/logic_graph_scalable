f55ff7daf4d8af9770975718503907bd3068816e
bushwallyta271828 <aheckett@gmail.com>
2024-04-23 11:39:19 -0400

    Updated analysis and documentation page.tsx files.

--- frontend/src/app/analysis/page.tsx
+++ frontend/src/app/analysis/page.tsx
@@ -1,7 +1,19 @@
+import { DebateLinks } from '@/app/_components/debate-links';
+
 export default function Analysis() {
   return (
-    <div className="bg-red-800 text-white">
-      <h1 className="text-xl">Welcome to the analysis of the claims!</h1>
-    </div>
+    <>
+      <nav className="bg-dark-neutral px-8 py-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div>
+            Analysis-specific content
+          </div>
+          <DebateLinks />
+        </div>
+      </nav>
+      <div className="bg-red-800 text-white">
+        <h1 className="text-xl">Welcome to the analysis of the claims!</h1>
+      </div>
+    </>
   );
 }
--- frontend/src/app/documentation/page.tsx
+++ frontend/src/app/documentation/page.tsx
@@ -1,9 +1,21 @@
+import { DebateLinks } from '@/app/_components/debate-links';
+
 export default function Documentation() {
   return (
-    <div className="bg-white text-black">
-      <h1 className="text-xl">Welcome to the documentation!</h1>
-      <p>Some content</p>
-      <p>Some more content</p>
-    </div>
+    <>
+      <nav className="bg-dark-neutral px-8 py-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div>
+            Documentation-specific content.
+          </div>
+          <DebateLinks />
+        </div>
+      </nav>
+      <div className="bg-white text-black">
+        <h1 className="text-xl">Welcome to the documentation!</h1>
+        <p>Some content</p>
+        <p>Some more content</p>
+      </div>
+    </>
   );
 }
