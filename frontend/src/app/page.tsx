8ffe7a87bfef42750132c7bb06ac0b1464e08465
bushwallyta271828 <aheckett@gmail.com>
2024-04-23 11:18:04 -0400

    Updated layout.tsx and page.tsx.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,19 @@
-import { ClaimList } from '@/app/_components/claim-list';
 import { CallAPI } from '@/app/_lib/call-api';
+import { NewClaimButton } from '@/app/_components/new-claim-button';
+import { DebateLinks } from '@/app/_components/debate-links';
+import { ClaimList } from '@/app/_components/claim-list';
 
 export default async function Home() {
   const responseText = await (await CallAPI()).text();
   console.log(responseText);
   
   return (
+    <nav className="bg-dark-neutral text-white text-lg font-bold px-8 py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <NewClaimButton />
+        <DebateLinks />
+      </div>
+    </nav>
     <ClaimList />
   );
 }
