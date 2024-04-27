3ffeafd11e35984ba4c7bf3aff4fd3d678a07c47
bushwallyta271828 <aheckett@gmail.com>
2024-04-27 16:56:16 -0400

    Removed "app." to accommodate new asgi.py file placement

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -7,4 +7,4 @@ python manage.py migrate
 
 PORT=${BACKEND_PORT:-80}
 
-uvicorn app.asgi:application --host 0.0.0.0 --port ${PORT} --workers 4
+uvicorn asgi:application --host 0.0.0.0 --port ${PORT} --workers 4
