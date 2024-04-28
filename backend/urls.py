74c138d25f9f408affbc444eda8bcd943312242f
bushwallyta271828 <aheckett@gmail.com>
2024-04-28 18:30:34 -0400

    Fixed bugs from changing accounts to users

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('users/', include('users.urls')),
     path('debates/', include('debates.urls')),
 ]
