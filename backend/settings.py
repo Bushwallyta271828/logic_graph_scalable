aba47c6411c48aac4f3d6c18e60751db9537ab6b
bushwallyta271828 <aheckett@gmail.com>
2024-05-04 16:17:09 -0400

    Fixed typo

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-extensions',
+    'django_extensions',
     'rest_framework',
     'corsheaders',
     'utils',
