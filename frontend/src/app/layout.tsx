4251d2e90382c273a65031e18afc6970c38d5cd8
bushwallyta271828 <aheckett@gmail.com>
2024-05-05 19:19:22 -0400

    I added a navbar to the root layout.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Link from 'next/link';
 import { UserContextProvider } from '@/app/_user_context/user-context';
+import { Navbar } from '@/app/navbar';
 
 export const metadata: Metadata = {
   title: 'Logic Graph',
@@ -11,6 +13,27 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
     <UserContextProvider>
       <html lang="en" className="scrollbar-thin scrollbar-track-dark-neutral scrollbar-thumb-medium-neutral">
         <body className="bg-medium-neutral min-h-screen">
+          <Navbar border={false}>
+            <div className="text-white text-lg font-bold flex gap-4">
+              <Link href="/">
+                <button className="bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
+                  LogicGraph
+                </button>
+              </Link>
+            </div>
+            <div className="text-white text-lg font-bold flex gap-4">
+              <Link href="/">
+                <button className="bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
+                  Account
+                </button>
+              </Link>
+              <Link href="/documentation">
+                <button className="bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
+                  Documentation
+                </button>
+              </Link>
+            </div>
+          </Navbar>
           {children}
         </body>
       </html>
