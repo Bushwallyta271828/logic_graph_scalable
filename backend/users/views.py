35a204ae19111b45a457dfeb5b45c2adc059028a
bushwallyta271828 <aheckett@gmail.com>
2024-05-05 18:24:39 -0400

    Added login_view

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,15 @@
+from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
-@api_view(['GET'])
-def getData(request):
-    claim = {'id': 1, 'statement': 'This message is from users!'}
-    return Response(claim)
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return Response({"message": "Logged in"}, status=status.HTTP_200_OK)
+    else:
+        return Response({"message": "Failed"}, status=status.HTTP_401_UNAUTHORIZED)
