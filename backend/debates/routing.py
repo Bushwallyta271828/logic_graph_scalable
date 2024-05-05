a2af87b131a0b7acec8c0805c65463817956ca9a
bushwallyta271828 <aheckett@gmail.com>
2024-05-05 11:48:58 -0400

    Adapted code to my setup

--- backend/debates/routing.py
+++ backend/debates/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/some_path/', consumers.MyConsumer.as_asgi()),
+    path('', consumers.DebateConsumer.as_asgi()),
 ]
