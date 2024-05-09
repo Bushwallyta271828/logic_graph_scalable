6d3a58904d21e8ef1056c18f19114e47160e2c55
bushwallyta271828 <aheckett@gmail.com>
2024-05-08 23:24:12 -0400

    I think api.tsx looks good now?

--- frontend/src/app/_lib/api.tsx
+++ frontend/src/app/_lib/api.tsx
@@ -2,53 +2,35 @@
 
 import { unstable_noStore as noStore } from 'next/cache';
 import { useSWR } from 'swr';
-import { useSWRMutation } from 'swr/mutation';
 
-export function useGet({path}: {path: string}) {
-  const get = async (path: string) => {
-    noStore(); //Don't store process.env.BACKEND_ADDRESS.
-    if (typeof process.env.BACKEND_ADDRESS === 'undefined') {
-      throw new Error('BACKEND_ADDRESS undefined');
-    } else {
-      try {
-        const response = fetch(process.env.BACKEND_ADDRESS + path, { cache: 'no-store' });
-        if (!response.ok) {
-          throw new Error('An error occurred while fetching data');
-        }
-        return response;
-      } catch {
-        throw new Error('Unable to fetch');
-      }
-    }
-  }
-
-  const { data, error, isValidating } = useSWR(path, get);
-  return {data, error, isValidating};
-}
-
-export async function post<T>({path, data}: {path: string, data: T}) {
+export async function fetchWrapper<T>({path, data}: {path: string, data: T | null}) {
+  //Call as fetchWrapper<null>(path: path, data: null) for GET,
+  //fetchWrapper<T>(path: path, data: T) for POST.
   noStore(); //Don't store process.env.BACKEND_ADDRESS.
   if (typeof process.env.BACKEND_ADDRESS === 'undefined') {
     throw new Error('BACKEND_ADDRESS undefined');
   } else {
     try {
-      const response = await fetch(process.env.BACKEND_ADDRESS + path, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-        cache: 'no-store',
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+      if (data === null) {
+        const response = await fetch(process.env.BACKEND_ADDRESS + path, { cache: 'no-store' });
+      } else {
+        const response = await fetch(process.env.BACKEND_ADDRESS + path, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+          cache: 'no-store',
+        });
       }
-
-      const { status, message } = await response.json();
-      return { status: status, message: message };
-    } catch (error) {
-      throw new Error('Failed to post data');
-    }
+      if (!response.ok) {throw new Error('Network response was not ok');}
+      return response;
+    } catch (error) {throw new Error('Unable to fetch');}
   }
+}
+
+export function useGet({path}: {path: string}) {
+  const get = async (path: string) => {return fetchWrapper<null>({path: path, data: null});}
+  const { data, error, isValidating } = useSWR(path, get);
+  return {data, error, isValidating};
 }
