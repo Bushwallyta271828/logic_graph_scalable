59b7202bc3220404cd01f2e635086458b6f63bd4
bushwallyta271828 <aheckett@gmail.com>
2024-05-10 13:27:31 -0400

    Removed useGet, SWR and switched back to server side fetching.

--- frontend/src/app/_lib/api.tsx
+++ frontend/src/app/_lib/api.tsx
@@ -1,14 +1,11 @@
-'use client';
-
 import { unstable_noStore as noStore } from 'next/cache';
-import useSWR from 'swr';
 
 export async function fetchWrapper<T>({path, data}: {path: string, data: T | null}) {
   //Call as fetchWrapper<null>(path: path, data: null) for GET,
   //fetchWrapper<T>(path: path, data: T) for POST.
-  noStore(); //Don't store process.env.NEXT_PUBLIC_BACKEND_ADDRESS.
-  if (typeof process.env.NEXT_PUBLIC_BACKEND_ADDRESS === 'undefined') {
-    throw new Error('NEXT_PUBLIC_BACKEND_ADDRESS undefined');
+  noStore(); //Don't store process.env.BACKEND_ADDRESS.
+  if (typeof process.env.BACKEND_ADDRESS === 'undefined') {
+    throw new Error('BACKEND_ADDRESS undefined');
   } else {
     try {
       const options: RequestInit = {cache: 'no-store'};
@@ -17,15 +14,9 @@ export async function fetchWrapper<T>({path, data}: {path: string, data: T | nul
         options.headers = {'Content-Type': 'application/json'};
         options.body = JSON.stringify(data);
       }
-      const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_ADDRESS + path, options);
+      const response = await fetch(process.env.BACKEND_ADDRESS + path, options);
       if (!response.ok) {throw new Error('Network response was not ok');}
       return response;
     } catch (error) {throw new Error('Unable to fetch');}
   }
-}
-
-export function useGet({path}: {path: string}) {
-  const get = async (path: string) => {return fetchWrapper<null>({path: path, data: null});}
-  const { data, error, isValidating } = useSWR(path, get);
-  return {data, error, isValidating};
 }
