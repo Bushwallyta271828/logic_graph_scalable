05c9ffefad7d48addf20d6be009925df312058be
bushwallyta271828 <aheckett@gmail.com>
2024-05-10 19:07:59 -0400

    Created signup_view

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view),
+    path('login', views.login_view),
+    path('signup', views.signup_view),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import User, authenticate, login
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -13,3 +13,16 @@ def login_view(request):
         return Response({"message": "Logged in"}, status=status.HTTP_200_OK)
     else:
         return Response({"message": "Failed"}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['POST'])
+def signup_view(request):
+    username = request.data.get('username')
+    email = request.data.get('email')
+    password = request.data.get('password')
+    
+    if User.objects.filter(username=username).exists():
+        return Response({"message": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        user = User.objects.create_user(username, email, password)
+        login(request, user)
+        return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
