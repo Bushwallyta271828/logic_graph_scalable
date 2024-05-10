41e630af83d2c3b10b02274ddd00367be266a380
bushwallyta271828 <aheckett@gmail.com>
2024-05-10 16:21:36 -0400

    Integrated post into login/page.tsx, doesn't work yet

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,11 +1,10 @@
+import { post } from '@/app/_lib/api';
+
 export default function Login() {
   async function submitLoginForm(formData: FormData) {
     'use server';
-    const username = formData.get('username');
-    const password = formData.get('password');
-  
-    console.log('Username:', username);
-    console.log('Password:', password);
+    const data = {username: formData.get('username'), password: formData.get('password')};
+    post({path: 'users/', data: data});
   }
 
   return (
