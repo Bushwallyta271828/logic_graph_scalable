1cfac81f98c6a1307067144e3ddfcb014a4959d3
bushwallyta271828 <aheckett@gmail.com>
2024-05-10 16:16:23 -0400

    I think Server Actions are working now?? I removed login-form.tsx and sign-up-form.tsx.

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,36 +1,24 @@
-import { LoginForm } from '@/app/login/login-form';
-import { SignUpForm } from '@/app/login/sign-up-form';
-
 export default function Login() {
-  //async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-  //  'use server';
-  //  event.preventDefault();
-  //
-  //  const formData = new FormData(event.currentTarget);
-  //  console.log(formData);
-  //}
-
-
-
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  async function submitLoginForm(formData: FormData) {
     'use server';
-    event.preventDefault();
-  
-    const formData = new FormData(event.currentTarget);
-    const username = formData.get('username'); // Get the username from the form
-    const password = formData.get('password'); // Get the password from the form
+    const username = formData.get('username');
+    const password = formData.get('password');
   
     console.log('Username:', username);
     console.log('Password:', password);
-  
-  };
+  }
 
   return (
-    <LoginForm handleSubmit={handleSubmit} />
-    //<form onSubmit={handleSubmit}>
-    //  <input type="text" name="name" />
-    //  <button type="submit">Submit</button>
-    //</form>
+    <form action={submitLoginForm}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
   );
 }
