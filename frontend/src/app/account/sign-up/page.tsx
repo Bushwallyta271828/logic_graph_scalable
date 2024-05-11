1bd7c660f5572d13049226831b058314ff8ca8ce
bushwallyta271828 <aheckett@gmail.com>
2024-05-11 17:54:04 -0400

    Worked on trying to create a cookie to save the username.

--- frontend/src/app/account/sign-up/page.tsx
+++ frontend/src/app/account/sign-up/page.tsx
@@ -1,10 +1,22 @@
 import { fetchWrapper } from '@/app/_lib/api';
+import { cookies } from 'next/headers';
 
 export default function SignUp() {
   async function submitSignUpForm(formData: FormData) {
     'use server';
     const response = await fetchWrapper(
       {path: 'users/sign-up', options: {method: 'POST', body: formData}});
+    const attemptSessionIdCookie = cookies().get('sessionid');
+    if (attemptSessionIdCookie !== undefined) {
+      const formUsername = formData.get('username');
+      if (typeof formUsername === 'string') {
+        cookies().set('username', formUsername, {
+          expires: attemptSessionIdCookie.expires,
+          maxAge: attemptSessionIdCookie.maxAge,
+          httpOnly: false,
+        });
+      } else {throw new Error("formData.get('username') isn't a string");}
+    }
     console.log(response);
   }
 
