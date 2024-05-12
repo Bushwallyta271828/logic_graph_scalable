3311cb8929f99667ce0f19d7b77a653300d8232d
bushwallyta271828 <aheckett@gmail.com>
2024-05-12 16:40:00 -0400

    Updated change_username_view

--- backend/users/views.py
+++ backend/users/views.py
@@ -30,16 +30,15 @@ def sign_up_view(request):
 
 @api_view(['POST'])
 def change_username_view(request):
-    username = request.data.get('username')
-    email = request.data.get('email')
-    password = request.data.get('password')
-    
-    if User.objects.filter(username=username).exists():
-        return Response({"message": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
+    if request.user.is_authenticated:
+        new_username = request.data.get('new-username')
+        if User.objects.filter(username=new_username).exists():
+            return Response({"message": "Username already taken"}, status=status.HTTP_400_BAD_REQUEST)
+        user.username = new_username
+        user.save()
+        return Response({"message": "Username changed successfully"}, status=status.HTTP_200_OK)
     else:
-        user = User.objects.create_user(username, email, password)
-        login(request, user)
-        return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response({"message": "Failed"}, status=status.HTTP_401_UNAUTHORIZED)
 
 @api_view(['POST'])
 def change_email_view(request):
