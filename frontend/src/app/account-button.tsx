b1a42350c7844e84794619cd0df4fd6ec32883d3
bushwallyta271828 <aheckett@gmail.com>
2024-05-11 21:47:37 -0400

    Fixed positioning issue (needed relative).

--- frontend/src/app/account-button.tsx
+++ frontend/src/app/account-button.tsx
@@ -56,20 +56,22 @@ function UsernameMenuItems({username}: {username: string}) {
 
 export function AccountButton({username}: {username: string | null}) {
   return (
-    <Menu>
-      <Menu.Button>
-        {({ open }) =>
-          open
-            ? (<p className="px-2 py-1 rounded-md bg-medium-neutral hover:bg-bright-neutral">Account</p>)
-            : (<p className="px-2 py-1 rounded-md bg-transparent hover:bg-medium-neutral">Account</p>)
-        }
-      </Menu.Button>
-      <Menu.Items className="absolute w-36 origin-top-right z-30 bg-transparent outline outline-1 outline-white rounded-md shadow-xl text-sm font-normal">
-        { (username === null) ?
-          <NoUsernameMenuItems /> :
-          <UsernameMenuItems username={username} />
-        }
-      </Menu.Items>
-    </Menu>
+    <div className="text-white text-lg font-bold relative">
+      <Menu>
+        <Menu.Button>
+          {({ open }) =>
+            open
+              ? (<p className="px-2 py-1 rounded-md bg-medium-neutral hover:bg-bright-neutral">Account</p>)
+              : (<p className="px-2 py-1 rounded-md bg-transparent hover:bg-medium-neutral">Account</p>)
+          }
+        </Menu.Button>
+        <Menu.Items className="absolute w-36 origin-top-right z-30 bg-transparent outline outline-1 outline-white rounded-md shadow-xl text-sm font-normal">
+          { (username === null) ?
+            <NoUsernameMenuItems /> :
+            <UsernameMenuItems username={username} />
+          }
+        </Menu.Items>
+      </Menu>
+    </div>
   );
 }
