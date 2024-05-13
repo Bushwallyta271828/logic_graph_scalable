0010c94f84fbfddab38612ed67acce868d510dcb
bushwallyta271828 <aheckett@gmail.com>
2024-05-13 17:58:18 -0400

    Added middleware to ignore CSRF, seems to work!

--- backend/settings.py
+++ backend/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'users.disable_csrf_middleware.DisableCSRFMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
