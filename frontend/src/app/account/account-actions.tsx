b6e63a6ab56377db21c9dcf0cdc13ef319a57cea
bushwallyta271828 <aheckett@gmail.com>
2024-05-13 18:03:51 -0400

    Moved fetch-wrapper.tsx outside of _lib and deleted _lib.

--- frontend/src/app/account/account-actions.tsx
+++ frontend/src/app/account/account-actions.tsx
@@ -3,7 +3,7 @@
 import { cookies } from 'next/headers';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { fetchWrapper } from '@/app/_lib/fetch-wrapper';
+import { fetchWrapper } from '@/app/fetch-wrapper';
 
 async function formDataToJSON(formData: FormData) {
   let formDataObj: Record<string, string> = {};
