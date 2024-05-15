0592c47059c4273d55af1bfe279143c86503eb9d
bushwallyta271828 <aheckett@gmail.com>
2024-05-14 21:17:16 -0400

    Separated helper function from fetchWrapper and added 'use server' commands inside helper functions.

--- frontend/src/app/api.tsx
+++ frontend/src/app/api.tsx
@@ -1,12 +1,41 @@
+'use server';
+
 import { unstable_noStore as noStore } from 'next/cache';
 import { cookies } from 'next/headers';
 import { parse, splitCookiesString } from 'set-cookie-parser';
+
+async function setHeaderCookies(headerValue: string, headerName: string) {
+  //Thanks to https://stackoverflow.com/a/77446172 for the ideas on cookie handling.
+
+  'use server'; //This shouldn't be needed but empirically is?
+  if (headerName.toLowerCase() === 'set-cookie') {
+    for (const cookieObject of parse(splitCookiesString(headerValue))) {
+      //Note: I have to unpack everything manually for type checking reasons.
+      //These properties are all of the properties that cookieObject can possess.
+      const sameSite = (cookieObject.sameSite === undefined)
+        ? undefined : cookieObject.sameSite.toLowerCase();
+      await (await cookies()).set({
+        name: cookieObject.name,
+        value: cookieObject.value,
+        path: cookieObject.path,
+        expires: cookieObject.expires,
+        maxAge: cookieObject.maxAge,
+        domain: cookieObject.domain,
+        secure: cookieObject.secure,
+        httpOnly: cookieObject.httpOnly,
+        sameSite: (sameSite as 'lax' | 'strict' | 'none' | boolean | undefined),
+      });
+    }
+  }
+}
 
 async function fetchWrapper({path, options = {}, headers = {}}:
   {path: string, options?: RequestInit, headers?: Record<string, string>}) {
   //options.headers and options.cache will be ignored.
   //options and headers may both be modified.
   //Note that this function is for server-side use only.
+
+  'use server'; //This shouldn't be needed but empirically is?
   noStore(); //Don't store process.env.BACKEND_ADDRESS.
   if (typeof process.env.BACKEND_ADDRESS === 'undefined') {
     throw new Error('BACKEND_ADDRESS undefined');
@@ -18,34 +47,12 @@ async function fetchWrapper({path, options = {}, headers = {}}:
         headers['Cookie'] = 'sessionid='+sessionidCookie.value;
       }
       options.headers = headers;
-
       const response = await fetch(process.env.BACKEND_ADDRESS + path, options);
 
-      //Thanks to https://stackoverflow.com/a/77446172 for the ideas on cookie handling.
-      response.headers.forEach((headerValue, headerName) => {
-        if (headerName.toLowerCase() === 'set-cookie') {
-          for (const cookieObject of parse(splitCookiesString(headerValue))) {
-            //Note: I have to unpack everything manually for type checking reasons.
-            //These properties are all of the properties that cookieObject can possess.
-            const sameSite = (cookieObject.sameSite === undefined)
-              ? undefined : cookieObject.sameSite.toLowerCase();
-            cookies().set({
-              name: cookieObject.name,
-              value: cookieObject.value,
-              path: cookieObject.path,
-              expires: cookieObject.expires,
-              maxAge: cookieObject.maxAge,
-              domain: cookieObject.domain,
-              secure: cookieObject.secure,
-              httpOnly: cookieObject.httpOnly,
-              sameSite: (sameSite as 'lax' | 'strict' | 'none' | boolean | undefined),
-            });
-          }
-        }
-      });
+      response.headers.forEach(setHeaderCookies);
 
       return response;
-    } catch (error) {throw new Error('Unable to fetch');}
+    } catch (error) {throw error;}
   }
 }
 
