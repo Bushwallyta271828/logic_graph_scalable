d7d8dcee33875a9a324716ea6885d2577f04389a
bushwallyta271828 <aheckett@gmail.com>
2024-05-15 10:22:45 -0400

    Created barebones RootLayout for compilation reasons

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Logic Graph',
+};
+
+export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
+  return ({children});
+}
