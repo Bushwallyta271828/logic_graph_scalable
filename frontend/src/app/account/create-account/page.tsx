66bacc2deb2672bb08e844c0272bd9321596ca31
bushwallyta271828 <aheckett@gmail.com>
2024-05-16 10:27:43 -0400

    Updated create-account page

--- frontend/src/app/account/create-account/page.tsx
+++ frontend/src/app/account/create-account/page.tsx
@@ -1,5 +1,24 @@
-import { CreateAccount } from '@/app/account/create-account/create-account';
+import { AccountForm } from '@/app/account/account-form';
 
-export default function CreateAccountPage() {
-  return (<CreateAccount />); //For client-side rendering
+export default function CreateAccount() {
+  //AccountForm is client-side
+  //redirect="/debates" ensures that AccountButton gets refreshed
+  //if the account creation is successful.
+  return (
+    <AccountForm path="users/create-account" redirect="/debates">
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" name="email" required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+      </div>
+      <button type="submit">Create Account</button>
+    </AccountForm>
+  );
 }
