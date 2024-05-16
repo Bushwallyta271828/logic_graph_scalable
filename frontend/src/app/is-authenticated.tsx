8165151675370b7b4526086a22da852759711284
bushwallyta271828 <aheckett@gmail.com>
2024-05-15 22:14:20 -0400

    Updated is-authenticated to use fetchWrapper

--- frontend/src/app/is-authenticated.tsx
+++ frontend/src/app/is-authenticated.tsx
@@ -1,20 +1,30 @@
 'use server';
 
 import { cookies } from 'next/headers';
-import { get } from '@/app/api';
+import { fetchWrapper } from '@/app/_api/fetch-wrapper';
+
+
+async function clearCookie(cookie: {name: string, value: string}) {
+  'use server'; //This command shouldn't be needed but empirically it is?
+  await (await cookies()).delete(cookie.name);
+}
 
 export async function isAuthenticated(): Promise<boolean | null> {
   //NOTE: This function should only ever be called from AccountButton!
   //If it were to be called in any other context, it would fail to update
   //the state of the AccountButton menu which could create an inconsistent
   //user interface!
-  'use server';
-  const response = await get({path: 'users/authenticated'});
+  'use server'; //This command shouldn't be needed but empirically it is?
+  const response = await fetchWrapper({
+    path: 'users/authenticated', //This path should only ever be accessed here!
+    deleteCookies: false,
+    redirectSignIn: false,
+  });
   if (response.ok) {
     const data = await response.json();
     if ('authenticated' in data && typeof data.authenticated === 'boolean') {
-      if (data.authenticated === false && await (await cookies()).has('sessionid'))
-        {await (await cookies()).delete('sessionid');}
+      if (data.authenticated === false)
+        {await (await (await cookies()).getAll()).map(clearCookie);}
       return data.authenticated;
     }
   }
