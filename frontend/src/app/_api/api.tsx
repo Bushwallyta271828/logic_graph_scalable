179c4196f1b5918a72070c15c6bd183d0612ec78
bushwallyta271828 <aheckett@gmail.com>
2024-05-15 23:08:13 -0400

    Updated api.tsx for router prop drilling

--- frontend/src/app/_api/api.tsx
+++ frontend/src/app/_api/api.tsx
@@ -1,42 +1,42 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { NextRouter } from 'react/router';
 import { fetchWrapper } from '@/app/_api/fetch-wrapper';
 
 
-function potentialRefresh(response: {error: string, status: number | null} | {data: any, status: number}) {
+function potentialRefresh({response, router}:
+  {response: {error: string, status: number | null} | {data: any, status: number}, router: NextRouter}) {
   //Resets AccountButton
   if (response.status === 401) {
-    router = useRouter();
     router.refresh();
   }
 }
 
-export async function get({path, deleteCookies = false, redirectSignIn = true}:
-  {path: string, deleteCookies?: boolean, redirectSignIn?: boolean}) {
+export async function get({path, router, deleteCookies = false, redirectSignIn = true}:
+  {path: string, router: NextRouter, deleteCookies?: boolean, redirectSignIn?: boolean}) {
   const response = await fetchWrapper({
     path: path,
     deleteCookies: deleteCookies,
     redirectSignIn: redirectSignIn,
   });
-  potentialRefresh(response);
+  potentialRefresh({response: response, router: router});
   return response;
 }
 
-export async function postForm({path, formData, deleteCookies = false, redirectSignIn = true}:
-  {path: string, formData: FormData, deleteCookies?: boolean, redirectSignIn?: boolean}) {
+export async function postForm({path, router, formData, deleteCookies = false, redirectSignIn = true}:
+  {path: string, router: NextRouter, formData: FormData, deleteCookies?: boolean, redirectSignIn?: boolean}) {
   const response = await fetchWrapper({
     path: path,
     options: {method: 'POST', body: formData},
     deleteCookies: deleteCookies,
     redirectSignIn: redirectSignIn,
   });
-  potentialRefresh(response);
+  potentialRefresh({response: response, router: router});
   return response;
 }
 
-export async function postJSON({path, data = "{}", deleteCookies = false, redirectSignIn = true}:
-  {path: string, data?: string, deleteCookies?: boolean, redirectSignIn?: boolean}) {
+export async function postJSON({path, router, data = "{}", deleteCookies = false, redirectSignIn = true}:
+  {path: string, router: NextRouter, data?: string, deleteCookies?: boolean, redirectSignIn?: boolean}) {
   const response = await fetchWrapper({
     path: path,
     options: {method: 'POST', body: data},
@@ -44,6 +44,6 @@ export async function postJSON({path, data = "{}", deleteCookies = false, redire
     deleteCookies: deleteCookies,
     redirectSignIn: redirectSignIn,
   });
-  potentialRefresh(response);
+  potentialRefresh({response: response, router: router});
   return response;
 }
