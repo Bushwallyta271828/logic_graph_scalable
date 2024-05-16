830f30bc5f0b346b40fbe4c4fee548bb520a3bce
bushwallyta271828 <aheckett@gmail.com>
2024-05-16 14:50:45 -0400

    Integrated getAccountDetails into useEffect while handling errors

--- frontend/src/app/account/manage-account/manage-account.tsx
+++ frontend/src/app/account/manage-account/manage-account.tsx
@@ -1,22 +1,30 @@
 'use client';
 
-//import { useRouter } from 'next/navigation';
-//import { get } from '@/app/_api/api';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { get } from '@/app/_api/api';
 import { AccountForm } from '@/app/account/account-form';
 
 
-//async function getAccountDetails({router}: {router: NextRouter}) {
-//  const response = await get({path: 'users/account-details', router: router});
-//  if (response.ok) {
-//    const data = await response.json();
-//    if ('username' in data && 'email' in data) {
-//      return {username: data.username, email: data.email};
-//    }
-//  }
-//  return {username: 'Fix me', email: 'Fix me'};
-//}
-
 export function ManageAccount() {
+  const [accountDetails, setAccountDetails] =
+    useState<{username: string, email: string} | {error: string} | 'loading'>('loading' as const);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAccountDetails = async () => {
+      const response = await get({path: "users/account-details", router: router});
+      if ('data' in response) {
+        if (
+          'username' in response.data && typeof response.data.username === 'string' &&
+          'email' in response.data && typeof response.data.email === 'string'
+        ) {setAccountDetails({username: response.data.username, email: response.data.email});}
+        else {setAccountDetails({error: "Unexpected API return type"});}
+      } else {setAccountDetails({error: response.error});}
+    };
+    checkAccountDetails();
+  }, []);
+
   return (
     <>
       <AccountForm path="users/change-username">
