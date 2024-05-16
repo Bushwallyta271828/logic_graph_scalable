93d691b03c2f52e2f73427b3bfc83ed203572064
bushwallyta271828 <aheckett@gmail.com>
2024-05-15 22:50:33 -0400

    Made create-account page sort of client-side (still no transitions)

---
+++ frontend/src/app/account/create-account/create-account.tsx
@@ -0,0 +1,21 @@
+import { submitCreateAccountForm } from '@/app/account/account-action-wrappers';
+
+export function CreateAccount() {
+  return (
+    <form action={submitCreateAccountForm}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" name="email" required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+      </div>
+      <button type="submit">Create Account</button>
+    </form>
+  );
+}
--- frontend/src/app/account/create-account/page.tsx
+++ frontend/src/app/account/create-account/page.tsx
@@ -1,21 +1,5 @@
-import { submitCreateAccountForm } from '@/app/account/account-actions';
+import { CreateAccount } from '@/app/account/create-account/create-account';
 
-export default function CreateAccount() {
-  return (
-    <form action={submitCreateAccountForm}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email" required />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required />
-      </div>
-      <button type="submit">Create Account</button>
-    </form>
-  );
+export default function CreateAccountPage() {
+  return (<CreateAccount />); //For client-side rendering
 }
