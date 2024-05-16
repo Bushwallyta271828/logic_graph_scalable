e2d2e57eae260d1cb5413f6ab561840a6674fe29
bushwallyta271828 <aheckett@gmail.com>
2024-05-16 10:03:40 -0400

    In theory, sign-in.tsx should handle errors correctly now.

--- frontend/src/app/account/sign-in/sign-in.tsx
+++ frontend/src/app/account/sign-in/sign-in.tsx
@@ -1,30 +1,43 @@
 'use client';
 
-import { submitSignInForm } from '@/app/account/account-action-wrappers';
-
-
-export async function submitSignInForm({formData, router}: {formData: FormData, router: NextRouter}) {
-  const response = await postForm({path: 'users/sign-in', formData: formData, router: router});
-  if (!('error' in response)) {
-    router = useRouter();
-    router.push('/debates');
-  }
-}
-
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { postForm } from '@/app/_api/api';
 
 
 export function SignIn() {
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+ 
+  const handleSignInSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const response = await postForm({
+      path: 'users/sign-in',
+      formData: new FormData(event.currentTarget),
+      router: router,
+    });
+    if ('error' in response) {
+      setError(response.error);
+    } else {
+      router.push('/debates'); //Moving from sign-in to debates refreshes AccountButton
+      setError(null);
+    }
+  }
+
   return (
-    <form action={submitSignInForm}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" required />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" required />
-      </div>
-      <button type="submit">Sign In</button>
-    </form>
+    <>
+      <form onSubmit={handleSignInSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+      {(error === null) ? null : <p>{error}</p>}
+    </>
   );
 }
