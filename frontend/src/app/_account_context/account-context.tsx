6a35bfaa721cdf1641115d0e6994bc74c2422515
bushwallyta271828 <aheckett@gmail.com>
2024-05-16 19:17:28 -0400

    Simplified account-context.tsx

--- frontend/src/app/_account_context/account-context.tsx
+++ frontend/src/app/_account_context/account-context.tsx
@@ -4,27 +4,19 @@
 
 import { createContext, useContext, useState } from 'react';
 
-type AccountContext = {
-  account: null | {username: string};
-  setAccount: (newAccount: null | {username: string}) => void;
-}
+export type Account = null | {username: string};
 
-export const AccountContext = createContext<AccountContext | null>(null);
+export const AccountContext =
+  createContext<{account: Account, setAccount: (newAccount: Account) => void} | null>(null);
 
 export function AccountContextProvider({ children }: { children: React.ReactNode }) {
-  const [account, setAccount] = useState<null | {username: string}>(null);
-
-  return (
-    <AccountContext.Provider value={{account,setAccount}}>
-      {children}
-    </AccountContext.Provider>
-  );
+  const [account, setAccount] = useState<Account>(null);
+  return (<AccountContext.Provider value={{account,setAccount}}>{children}</AccountContext.Provider>);
 }
 
 export function useAccountContext() {
   const context = useContext(AccountContext);
-  if (context === null) {
-    throw new Error("useAccountContext must be used within an AccountContextProvider");
-  }
+  if (context === null)
+    {throw new Error("useAccountContext must be used within an AccountContextProvider");}
   return context;
 }
