8ec1af1dd64ca38165180e46640e7db5cba0b56a
bushwallyta271828 <aheckett@gmail.com>
2024-05-16 10:44:57 -0400

    Updated manage-account to use AccountForm, still haven't integrated getAccountDetails

--- frontend/src/app/account/manage-account/manage-account.tsx
+++ frontend/src/app/account/manage-account/manage-account.tsx
@@ -1,62 +1,45 @@
-import {
-  submitChangeUsernameForm,
-  submitChangeEmailForm,
-  submitChangePasswordForm,
-} from '@/app/account/account-action-wrappers';
+'use client';
+
+//import { useRouter } from 'next/router';
+//import { get } from '@/app/_api/api';
+import { AccountForm } from '@/app/account/account-form';
 
 
-
-export async function getAccountDetails({router}: {router: NextRouter}) {
-  const response = await get({path: 'users/account-details', router: router});
-  if (response.ok) {
-    const data = await response.json();
-    if ('username' in data && 'email' in data) {
-      return {username: data.username, email: data.email};
-    }
-  }
-  return {username: 'Fix me', email: 'Fix me'};
-}
-
-export async function submitChangeUsernameForm({formData, router}: {formData: FormData, router: NextRouter}) {
-  const response = await postForm({path: 'users/change-username', formData: formData, router: router});
-}
-
-export async function submitChangeEmailForm({formData, router}: {formData: FormData, router: NextRouter}) {
-  const response = await postForm({path: 'users/change-email', formData: formData, router: router});
-}
-
-export async function submitChangePasswordForm({formData, router}: {formData: FormData, router: NextRouter}) {
-  const response = await postForm({path: 'users/change-password', formData: formData, router: router});
-}
-
-
-
-
+//async function getAccountDetails({router}: {router: NextRouter}) {
+//  const response = await get({path: 'users/account-details', router: router});
+//  if (response.ok) {
+//    const data = await response.json();
+//    if ('username' in data && 'email' in data) {
+//      return {username: data.username, email: data.email};
+//    }
+//  }
+//  return {username: 'Fix me', email: 'Fix me'};
+//}
 
 export function ManageAccount() {
   return (
     <>
-      <form action={submitChangeUsernameForm}>
+      <AccountForm path="users/change-username">
         <div>
           <label htmlFor="new-username">New Username:</label>
           <input type="text" id="new-username" name="new-username" required />
         </div>
         <button type="submit">Update Username</button>
-      </form>
-      <form action={submitChangeEmailForm}>
+      </AccountForm>
+      <AccountForm path="users/change-email">
         <div>
           <label htmlFor="new-email">New Email:</label>
           <input type="text" id="new-email" name="new-email" required />
         </div>
         <button type="submit">Update Email</button>
-      </form>
-      <form action={submitChangePasswordForm}>
+      </AccountForm>
+      <AccountForm path="users/change-password">
         <div>
           <label htmlFor="new-password">New Password:</label>
           <input type="password" id="new-password" name="new-password" required />
         </div>
         <button type="submit">Update Password</button>
-      </form>
+      </AccountForm>
     </>
   );
 }
