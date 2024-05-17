c8b892c420e103972dc8c76fe7f8b51f0e2bc271
bushwallyta271828 <aheckett@gmail.com>
2024-05-16 22:06:55 -0400

    Changed getAccountClient to refreshAccount

--- frontend/src/app/_api/refresh-account.tsx
+++ frontend/src/app/_api/refresh-account.tsx
@@ -4,9 +4,7 @@ import { getAccountDetailsAction } from '@/app/_api/get-account-details-action';
 import { Account, SetAccount } from '@/app/_account_context/account-context';
 
 
-export async function getAccountClient({setAccount}: {setAccount: SetAccount}):
-  Promise<{account: Account} | null> {
+export async function refreshAccount({setAccount}: {setAccount: SetAccount}) {
   const attemptedAccount = await getAccountDetailsAction();
   if ('account' in attemptedAccount) {setAccount(attemptedAccount.account);}
-  return attemptedAccount;
 }
