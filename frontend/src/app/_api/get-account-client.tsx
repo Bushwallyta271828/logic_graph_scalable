3b8eb2a1bcb41544634c7cc301da77a657c0f01f
bushwallyta271828 <aheckett@gmail.com>
2024-05-16 22:02:32 -0400

    Wrote getAccountClient

--- frontend/src/app/_api/get-account-client.tsx
+++ frontend/src/app/_api/get-account-client.tsx
@@ -6,17 +6,7 @@ import { Account, SetAccount } from '@/app/_account_context/account-context';
 
 export async function getAccountClient({setAccount}: {setAccount: SetAccount}):
   Promise<{account: Account} | null> {
-  const 
-  const response = await fetchWrapper({
-    path: 'users/authenticated', //This path should only ever be accessed here!
-    deleteCookies: false,
-  });
-  if ('data' in response) {
-    if ('authenticated' in response.data && typeof response.data.authenticated === 'boolean') {
-      if (response.data.authenticated === false)
-        {await (await (await cookies()).getAll()).map(clearCookie);}
-      return response.data.authenticated;
-    }
-  }
-  return null;
+  const attemptedAccount = await getAccountDetailsAction();
+  if ('account' in attemptedAccount) {setAccount(attemptedAccount.account);}
+  return attemptedAccount;
 }
