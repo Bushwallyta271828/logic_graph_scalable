8102c736d498d470f7dbe441fa04a7106ae89d90
bushwallyta271828 <aheckett@gmail.com>
2024-05-18 12:10:47 -0400

    Updated api functions to setAccount to signed out if forceSignOut (formerly deleteCookies) is true.

--- frontend/src/app/_api/api.tsx
+++ frontend/src/app/_api/api.tsx
@@ -10,47 +10,48 @@ interface PartialRouter {push: (href: string) => void;}
  * path is the path of the API to call.
  * setAccount should update the account state in case of an authentication error.
  * If a router is provided, then authentication errors will re-direct to the sign-in page.
- * If deleteCookies === true then all cookies will be deleted, regardless of request outcome.
+ * If forceSignOut === true then all cookies will be deleted and the user will be signed out,
+ * regardless of request outcome.
  */
 
-export async function get({path, setAccount, router, deleteCookies = false}:
-  {path: string, setAccount: SetAccount, router?: PartialRouter, deleteCookies?: boolean}) {
+export async function get({path, setAccount, router, forceSignOut = false}:
+  {path: string, setAccount: SetAccount, router?: PartialRouter, forceSignOut?: boolean}) {
   const response = await fetchWrapper({
     path: path,
     deleteCookies: deleteCookies,
   });
-  if (response.status === 401) {
-    if (router !== undefined) {router.push('/account/sign-in');}
-    setAccount('signed out' as const);
-  }
+  if (response.status === 401 && router !== undefined)
+    {router.push('/account/sign-in');}
+  if (response.status === 401 || forceSignOut)
+    {setAccount('signed out' as const);}
   return response;
 }
 
-export async function postForm({path, formData, setAccount, router, deleteCookies = false}:
-  {path: string, formData: FormData, setAccount: SetAccount, router?: PartialRouter, deleteCookies?: boolean}) {
+export async function postForm({path, formData, setAccount, router, forceSignOut = false}:
+  {path: string, formData: FormData, setAccount: SetAccount, router?: PartialRouter, forceSignOut?: boolean}) {
   const response = await fetchWrapper({
     path: path,
     options: {method: 'POST', body: formData},
     deleteCookies: deleteCookies,
   });
-  if (response.status === 401) {
-    if (router !== undefined) {router.push('/account/sign-in');}
-    setAccount('signed out' as const);
-  }
+  if (response.status === 401 && router !== undefined)
+    {router.push('/account/sign-in');}
+  if (response.status === 401 || forceSignOut)
+    {setAccount('signed out' as const);}
   return response;
 }
 
-export async function postJSON({path, data, setAccount, router, deleteCookies = false}:
-  {path: string, data: string, setAccount: SetAccount, router?: PartialRouter, deleteCookies?: boolean}) {
+export async function postJSON({path, data, setAccount, router, forceSignOut = false}:
+  {path: string, data: string, setAccount: SetAccount, router?: PartialRouter, forceSignOut?: boolean}) {
   const response = await fetchWrapper({
     path: path,
     options: {method: 'POST', body: data},
     headers: {'Content-Type': 'application/json'},
     deleteCookies: deleteCookies,
   });
-  if (response.status === 401) {
-    if (router !== undefined) {router.push('/account/sign-in');}
-    setAccount('signed out' as const);
-  }
+  if (response.status === 401 && router !== undefined)
+    {router.push('/account/sign-in');}
+  if (response.status === 401 || forceSignOut)
+    {setAccount('signed out' as const);}
   return response;
 }
