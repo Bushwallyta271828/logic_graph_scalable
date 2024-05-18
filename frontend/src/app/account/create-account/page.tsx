62c962d86e88d5155fced23be3f8fe5fa105d8ae
bushwallyta271828 <aheckett@gmail.com>
2024-05-18 16:15:22 -0400

    Updated create-account page with usernameField

--- frontend/src/app/account/create-account/page.tsx
+++ frontend/src/app/account/create-account/page.tsx
@@ -1,5 +1,18 @@
-import { CreateAccount } from '@/app/account/create-account/create-account';
+import { AccountForm } from '@/app/account/account-form';
 
-export default function CreateAccountPage() {
-  return (<CreateAccount />);
+
+export default function CreateAccount() {
+  return (
+    <AccountForm path="users/create-account" redirectSignIn={false} usernameField="username" redirectOnSuccess="/debates">
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+      </div>
+      <button type="submit">Create Account</button>
+    </AccountForm>
+  );
 }
