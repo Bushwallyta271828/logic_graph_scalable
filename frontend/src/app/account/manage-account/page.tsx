5f7af577381d6b8355d4e766d63ea83c85e1e057
bushwallyta271828 <aheckett@gmail.com>
2024-05-18 18:57:59 -0400

    Moved manage-account.tsx to page.tsx thanks to DisplayUsername client component

--- frontend/src/app/account/manage-account/page.tsx
+++ frontend/src/app/account/manage-account/page.tsx
@@ -1,5 +1,26 @@
-import { ManageAccount } from '@/app/account/manage-account/manage-account';
+import { AccountForm } from '@/app/account/account-form';
+import { DisplayUsername } from '@/app/account/manage-account/display-username';
+import { PasswordInput } from '@/app/account/password-input';
 
-export default function ManageAccountPage() {
-  return (<ManageAccount />); //For client-side rendering
+
+export default function ManageAccount() {
+  return (
+    <>
+      <AccountForm path="users/change-username" redirectSignIn={true} usernameField="new-username">
+        <DisplayUsername />
+        <div>
+          <label htmlFor="new-username">New Username:</label>
+          <input type="text" id="new-username" name="new-username" autoComplete="off" required />
+        </div>
+        <button type="submit">Update Username</button>
+      </AccountForm>
+      <AccountForm path="users/change-password" redirectSignIn={true}>
+        <div>
+          <label htmlFor="new-password">New Password:</label>
+	  <PasswordInput name="new-password" autocomplete={false} />
+        </div>
+        <button type="submit">Update Password</button>
+      </AccountForm>
+    </>
+  );
 }
