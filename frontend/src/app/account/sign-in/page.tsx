5e11c6c15dbff4136a14bac64960d66c171c13e1
bushwallyta271828 <aheckett@gmail.com>
2024-05-18 16:10:04 -0400

    Switched sign-in back to server side thanks to usernameField

--- frontend/src/app/account/sign-in/page.tsx
+++ frontend/src/app/account/sign-in/page.tsx
@@ -1,5 +1,17 @@
-import { SignIn } from '@/app/account/sign-in/sign-in';
+import { AccountForm } from '@/app/account/account-form';
 
-export default function SignInPage() {
-  return (<SignIn />);
+export default function SignIn() {
+  return (
+    <AccountForm path="users/sign-in" redirectSignIn={false} usernameField="username" redirectOnSuccess="/debates">
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+      </div>
+      <button type="submit">Sign In</button>
+    </AccountForm>
+  );
 }
