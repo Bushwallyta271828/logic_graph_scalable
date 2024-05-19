461214017b4f4ab4ad639ed10a9120e08aac3f0a
bushwallyta271828 <aheckett@gmail.com>
2024-05-19 15:12:00 -0400

    Updated styling on DisplayUsername and ManageAccount

--- frontend/src/app/account/manage-account/display-username.tsx
+++ frontend/src/app/account/manage-account/display-username.tsx
@@ -6,8 +6,11 @@ import { useAccountContext } from '@/app/_account_context/account-context';
 export function DisplayUsername() {
   const { account } = useAccountContext();
 
-  if (account.status === 'loading') {return (<p className="italic">Loading username...</p>);}
-  else if (account.status === 'error') {return (<p className="text-bright-danger">Error: {account.error}</p>);}
-  else if (account.status === 'signed out') {return (<p>You currently appear to be signed out.</p>);}
-  else {return (<p>Current username: {account.username}</p>);}
+  return ({
+    (account.status === 'loading') ? <p className="text-white text-sm italic">Loading username...</p> :
+    (account.status === 'error') ?
+      <p className="text-bright-danger text-sm">Error while determining username: {account.error}</p> :
+    (account.status === 'signed out') ? <p className="text-white text-sm">You currently appear to be signed out.</p> :
+    <p className="text-white text-sm">Your current username is {account.username}</p>
+  });
 }
