b853c85b57e249eb9d4baa2bebf220d468d037f5
bushwallyta271828 <aheckett@gmail.com>
2024-05-19 12:34:35 -0400

    Improved the sign-in page styling a little bit more

--- frontend/src/app/account/sign-in/page.tsx
+++ frontend/src/app/account/sign-in/page.tsx
@@ -4,7 +4,7 @@ import { UsernameInput, PasswordInput } from '@/app/account/form-inputs';
 export default function SignIn() {
   return (
     <div>
-      <p className="text-white text-lg font-bold">Sign In</p>
+      <p className="text-white text-lg font-bold">Welcome Back!</p>
       <AccountForm path="users/sign-in" redirectSignIn={false} usernameField="username" redirectOnSuccess="/debates">
         <div className="flex flex-row items-baseline">
           <label htmlFor="username" className="w-20 text-white text-sm">Username:</label>
@@ -14,7 +14,7 @@ export default function SignIn() {
           <label htmlFor="password" className="w-20 text-white text-sm">Password:</label>
           <PasswordInput name="password" autocomplete={true} />
         </div>
-        <button type="submit" className="bg-bright-neutral hover:bg-dark-neutral text-white text-sm px-2 py-1 rounded-md">Submit</button>
+        <button type="submit" className="bg-bright-neutral text-white text-sm px-2 py-1 rounded-md">Sign In</button>
       </AccountForm>
     </div>
   );
