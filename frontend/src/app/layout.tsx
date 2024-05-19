35eed0f936bf3973763d2a011f1e1f5c4cbd2ebc
bushwallyta271828 <aheckett@gmail.com>
2024-05-19 13:31:51 -0400

    Moved AccountContextProvider outside html, body
    I want to try using a CSS grid layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,9 +13,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
-    <html lang="en" className="scrollbar-thin scrollbar-track-dark-neutral scrollbar-thumb-medium-neutral">
-      <body className="bg-medium-neutral min-h-screen">
-        <AccountContextProvider>
+    <AccountContextProvider>
+      <html lang="en" className="scrollbar-thin scrollbar-track-dark-neutral scrollbar-thumb-medium-neutral">
+        <body className="bg-medium-neutral min-h-screen">
           <Navbar border={false}>
             <div className="flex gap-4 items-baseline">
               <Link href="/">
@@ -40,8 +40,8 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
             </div>
           </Navbar>
           {children}
-        </AccountContextProvider>
-      </body>
-    </html>
+        </body>
+      </html>
+    </AccountContextProvider>
   );
 }
