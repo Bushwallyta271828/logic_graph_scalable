29ee454ad08fa168a285bc6880b63c526ea845b6
bushwallyta271828 <aheckett@gmail.com>
2024-05-19 14:49:41 -0400

    Applied sign-in styling to create-account page

--- frontend/src/app/account/create-account/page.tsx
+++ frontend/src/app/account/create-account/page.tsx
@@ -1,19 +1,21 @@
+import { FormCard } from '@/app/account/form-card';
 import { AccountForm } from '@/app/account/account-form';
 import { UsernameInput, PasswordInput } from '@/app/account/form-inputs';
 
 
 export default function CreateAccount() {
   return (
-    <AccountForm path="users/create-account" redirectSignIn={false} usernameField="username" redirectOnSuccess="/debates">
-      <div>
-        <label htmlFor="username">Username:</label>
+    <FormCard>
+      <p className="text-white text-lg font-bold mb-4 text-center">Welcome to LogicGraph!</p>
+      <AccountForm path="users/create-account" redirectSignIn={false} usernameField="username" redirectOnSuccess="/debates">
+        <label htmlFor="username" className="text-white text-sm">Username:</label>
         <UsernameInput name="username" autocomplete={false} />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password" className="text-white text-sm">Password:</label>
         <PasswordInput name="password" autocomplete={false} />
-      </div>
-      <button type="submit">Create Account</button>
-    </AccountForm>
+        <button type="submit" className="bg-medium-neutral hover:bg-bright-neutral text-white text-sm px-4 py-2 rounded-md w-full">
+          Create Account
+        </button>
+      </AccountForm>
+    </FormCard>
   );
 }
