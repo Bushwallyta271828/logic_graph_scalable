b31663329dcb2785ae15718b2e1f6bafb00c9da5
bushwallyta271828 <aheckett@gmail.com>
2024-05-19 10:24:53 -0400

    Added react-icons for password eye

--- frontend/src/app/account/password-input.tsx
+++ frontend/src/app/account/password-input.tsx
@@ -1,13 +1,19 @@
 'use client';
 
+import { IoEye, IoEyeOff } from "react-icons/io5";
+
 export function PasswordInput({name, autocomplete}: {name: string, autocomplete: boolean}) {
   return (
-    <input
-      type="password"
-      id={name}
-      name={name}
-      autoComplete={autocomplete ? "current-password" : "new-password"}
-      required
-    />
+    <>
+      <input
+        type="password"
+        id={name}
+        name={name}
+        autoComplete={autocomplete ? "current-password" : "new-password"}
+        required
+      />
+      <IoEye />
+      <IoEyeOff />
+    </>
   );
 }
