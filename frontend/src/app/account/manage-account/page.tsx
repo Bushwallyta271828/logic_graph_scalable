beebcec0876bc89ada53d9c918b994f9348e59f1
bushwallyta271828 <aheckett@gmail.com>
2024-05-19 15:02:49 -0400

    Started styling manage-account page

--- frontend/src/app/account/manage-account/page.tsx
+++ frontend/src/app/account/manage-account/page.tsx
@@ -1,26 +1,30 @@
+import { FormCard } from '@/app/account/form-card';
 import { AccountForm } from '@/app/account/account-form';
-import { DisplayUsername } from '@/app/account/manage-account/display-username';
 import { UsernameInput, PasswordInput } from '@/app/account/form-inputs';
+import { DisplayUsername } from '@/app/account/manage-account/display-username';
 
 
 export default function ManageAccount() {
   return (
-    <>
+    <FormCard>
+      <p className="text-white text-lg font-bold mb-4 text-center">Username</p>
+      <DisplayUsername />
+      <p className="text-white text-lg font-bold mb-4 text-center">Change Username</p>
       <AccountForm path="users/change-username" redirectSignIn={true} usernameField="new-username">
-        <DisplayUsername />
-        <div>
-          <label htmlFor="new-username">New Username:</label>
-          <UsernameInput name="new-username" autocomplete={false} />
-        </div>
-        <button type="submit">Update Username</button>
+        <label htmlFor="new-username" className="text-white text-sm">New Username:</label>
+        <UsernameInput name="new-username" autocomplete={false} />
+        <button type="submit" className="bg-medium-neutral hover:bg-bright-neutral text-white text-sm px-4 py-2 rounded-md w-full">
+          Update Username
+        </button>
       </AccountForm>
+      <p className="text-white text-lg font-bold mb-4 text-center">Change Password</p>
       <AccountForm path="users/change-password" redirectSignIn={true}>
-        <div>
-          <label htmlFor="new-password">New Password:</label>
-          <PasswordInput name="new-password" autocomplete={false} />
-        </div>
-        <button type="submit">Update Password</button>
+        <label htmlFor="new-password" className="text-white text-sm">New Password:</label>
+        <PasswordInput name="new-password" autocomplete={false} />
+        <button type="submit" className="bg-medium-neutral hover:bg-bright-neutral text-white text-sm px-4 py-2 rounded-md w-full">
+          Update Password
+        </button>
       </AccountForm>
-    </>
+    </FormCard>
   );
 }
