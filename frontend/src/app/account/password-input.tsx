10f3b5bf7cbc99a2b602d2ddd03040ec720fb36e
bushwallyta271828 <aheckett@gmail.com>
2024-05-19 11:39:37 -0400

    PasswordInput now uses input arguments

--- frontend/src/app/account/password-input.tsx
+++ frontend/src/app/account/password-input.tsx
@@ -6,21 +6,24 @@ import { IoEye, IoEyeOff } from 'react-icons/io5';
 export function PasswordInput({name, autocomplete}: {name: string, autocomplete: boolean}) {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
   return (
     <div className="relative w-80">
       <input
         type={passwordVisible ? 'text' : 'password'}
+        id={name}
+        name={name}
+        autoComplete = {autocomplete ? 'current-password' : 'new-password'}
+        required
         className="w-full p-1 pr-8 bg-bright-neutral text-white rounded-md outline-none"
       />
       <div
         className="absolute inset-y-0 right-0 flex items-center pr-2 cursor-pointer"
-        onClick={togglePasswordVisibility}
+        onClick={() => setPasswordVisible(!passwordVisible)}
       >
-        {passwordVisible ? <IoEyeOff size={16} className="text-white" /> : <IoEye size={16} className="text-white" />}
+        {passwordVisible ?
+          <IoEyeOff size={16} className="text-white" /> :
+          <IoEye size={16} className="text-white" />
+        }
       </div>
     </div>
   );
