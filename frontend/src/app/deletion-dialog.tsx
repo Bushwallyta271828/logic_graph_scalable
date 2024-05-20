76b259648029a396bae08fcd2f908490317b9cf9
bushwallyta271828 <aheckett@gmail.com>
2024-05-19 20:12:15 -0400

    Generalized DeletionDialog to new role

--- frontend/src/app/deletion-dialog.tsx
+++ frontend/src/app/deletion-dialog.tsx
@@ -1,44 +1,25 @@
 'use client';
 
 import { Dialog } from '@headlessui/react';
-import { Claim } from '@/app/debates/[debateID]/_debate_context/claim-types';
-import { useDebateContext } from '@/app/debates/[debateID]/_debate_context/debate-context';
 
 
-export function DeletionDialog({dialogOpen, setDialogOpen, claim, additionalClaims}: {
+export function DeletionDialog({title, dialogOpen, setDialogOpen, onDelete, children}: {
+  title: string,
   dialogOpen: boolean,
   setDialogOpen: React.Dispatch<React.SetStateAction<boolean>>,
-  claim: Claim,
-  additionalClaims: Claim[],
+  onDelete: () => void,
+  children: React.ReactNode,
 }) {
-  const { deleteClaim, getDisplayData } = useDebateContext();
-
-  const claimsDisplay = additionalClaims.slice(0, 8).map((additionalClaim) => {
-    const {displayText, validText} = getDisplayData(additionalClaim);
-    if (validText === true) {return additionalClaim.claimID + ": " + displayText;}
-    else {return additionalClaim.claimID + ": " + additionalClaim.text;}
-  });
-  if (additionalClaims.length > 8) {claimsDisplay.push("...");}
-
   return (
     <Dialog
-      data-no-dnd="true"
       open={dialogOpen}
       onClose={() => setDialogOpen(false)}
       className="relative z-50">
       <div className="fixed inset-0 backdrop-brightness-50 backdrop-blur" aria-hidden="true" />
       <div className="fixed inset-0 flex w-screen items-center justify-center">
         <Dialog.Panel className="mx-auto max-w-lg rounded-md outline outline-2 outline-white bg-dark-neutral text-white p-2">
-          <Dialog.Title className="text-center text-lg font-bold">Deleting Additional Claims</Dialog.Title>
-          <p>
-            By deleting this claim, you will also be deleting the following claims which depend upon it:
-          </p>
-          <div className="p-2">
-            { claimsDisplay.map((claimDisplay, index) => (
-                <p className="text-nowrap truncate" key={index}>{claimDisplay}</p>
-              ))
-            }
-          </div>
+          <Dialog.Title className="text-center text-lg font-bold">{title}</Dialog.Title>
+          {children}
           <p>Are you sure you wish to proceed?</p>
           <div className="container mx-auto flex justify-between items-center p-2">
             <button
@@ -48,7 +29,7 @@ export function DeletionDialog({dialogOpen, setDialogOpen, claim, additionalClai
             </button>
             <button
               className="bg-medium-danger hover:bg-bright-danger text-lg font-bold px-2 py-1 rounded-md"
-              onClick={() => deleteClaim(claim)}>
+              onClick={onDelete}>
               Delete
             </button>
           </div>
