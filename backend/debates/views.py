b47fd75ae79c54ccaa263b1a60137913c2fa3eb0
bushwallyta271828 <aheckett@gmail.com>
2024-05-23 11:49:17 -0400

    Updated debate urls and views

--- backend/debates/views.py
+++ backend/debates/views.py
@@ -6,6 +6,6 @@ from users.decorators import require_authentication
 
 @api_view(['GET'])
 @require_authentication
-def getDebate(request):
+def getDebate(request, debate_id):
     claim = {'id': 1, 'statement': 'This message is from debates!'}
     return Response(claim)
