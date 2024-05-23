0d1e53103b6fbf67bf4fdb06cf9fbbd8b7c1bd71
bushwallyta271828 <aheckett@gmail.com>
2024-05-23 10:44:40 -0400

    Set up debates/views.py with more imports

--- backend/debates/views.py
+++ backend/debates/views.py
@@ -1,7 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+from users.decorators import require_authentication
+
 
 @api_view(['GET'])
-def getData(request):
+@require_authentication
+def getDebate(request):
     claim = {'id': 1, 'statement': 'This message is from debates!'}
     return Response(claim)
