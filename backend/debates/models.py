bfe312c79e1f5b3873753b15b84e7636e0c3ba91
bushwallyta271828 <aheckett@gmail.com>
2024-05-23 10:29:13 -0400

    Expanded Debate model

--- backend/debates/models.py
+++ backend/debates/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Debate(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='debates')
+
+    def __str__(self):
+        return self.title
