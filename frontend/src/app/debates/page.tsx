8f354619ecd78264957f881c525363c40ede2dd3
bushwallyta271828 <aheckett@gmail.com>
2024-05-25 21:13:39 -0400

    Updated page.tsx to call DebateList

--- frontend/src/app/debates/page.tsx
+++ frontend/src/app/debates/page.tsx
@@ -1,7 +1,7 @@
+import { DebateList } from '@/app/debates/debate-list';
+
 export default function BrowseDebates() {
   return (
-    <div className="bg-red-800 text-white">
-      <h1 className="text-xl">Hmmm, it would probably be a good idea for someone to implement me at some point.</h1>
-    </div>
+    <DebateList />
   );
 }
