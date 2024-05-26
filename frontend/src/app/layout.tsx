988f11e4a63e0dd668d538cc500e27ba4181bf0c
bushwallyta271828 <aheckett@gmail.com>
2024-05-25 21:00:02 -0400

    Removed most things from layout.tsx.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import Link from 'next/link';
 import { AccountContextProvider } from '@/app/_account_context/account-context';
-import { Navbar } from '@/app/navbar';
-import { DebateName } from '@/app/debate-name';
-import { AccountButton } from '@/app/account-button';
 
 export const metadata: Metadata = {
   title: 'Logic Graph',
@@ -15,33 +11,8 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
   return (
     <AccountContextProvider>
       <html lang="en" className="scrollbar-thin scrollbar-track-dark-neutral scrollbar-thumb-medium-neutral">
-        <body className="bg-medium-neutral grid grid-rows-[auto_1fr] min-h-screen">
-          <Navbar border={false}>
-            <div className="flex gap-4 items-baseline">
-              <Link href="/">
-                <button className="text-white text-lg font-bold bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
-                  LogicGraph
-                </button>
-              </Link>
-              <Link href="/debates">
-                <button className="text-white text-lg font-bold bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
-                  Debates
-                </button>
-              </Link>
-              <DebateName />
-            </div>
-            <div className="text-white text-lg font-bold flex gap-4">
-              <AccountButton />
-              <Link href="/documentation">
-                <button className="bg-transparent hover:bg-medium-neutral px-2 py-1 rounded-md">
-                  Documentation
-                </button>
-              </Link>
-            </div>
-          </Navbar>
-          <div>
-            {children}
-          </div>
+        <body className="bg-medium-neutral">
+          {children}
         </body>
       </html>
     </AccountContextProvider>
