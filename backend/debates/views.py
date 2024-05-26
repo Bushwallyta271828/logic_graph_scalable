ced62bde2ac575cc5f1a99d2a21b589828202277
bushwallyta271828 <aheckett@gmail.com>
2024-05-25 20:20:23 -0400

    Switched to a different ChatGPT-provided views.py example

--- backend/debates/views.py
+++ backend/debates/views.py
@@ -1,22 +1,21 @@
-from rest_framework import viewsets, permissions
+from rest_framework import generics, permissions
 from .models import Debate
 from .serializers import DebateSerializer
-from rest_framework.decorators import action
-from rest_framework.response import Response
+from .permissions import IsOwner
 
-class DebateViewSet(viewsets.ModelViewSet):
-    queryset = Debate.objects.all()
+class DebateListCreateView(generics.ListCreateAPIView):
     serializer_class = DebateSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user)
+        return Debate.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    @action(detail=False, methods=['get'])
-    def my_debates(self, request):
-        debates = self.get_queryset()
-        serializer = self.get_serializer(debates, many=True)
-        return Response(serializer.data)
+class DebateDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = DebateSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Debate.objects.filter(user=self.request.user)
